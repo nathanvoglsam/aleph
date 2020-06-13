fb64a67882ae6b4c20448df814526897c961d831
Nathan Voglsam <nathanvoglsam420@gmail.com>
2020-06-14 01:54:37 +1000

    Fixed disassemble script

--- aleph-engine/shaders/disasm.bat
+++ aleph-engine/shaders/disasm.bat
@@ -1,7 +1,7 @@
 @echo off
-spirv-dis .\compiled\standard\standard.frag.spv >> .\disasm\standard\standard.frag.spirv
-spirv-dis .\compiled\standard\standard.vert.spv >> .\disasm\standard\standard.vert.spirv
-spirv-dis .\compiled\standard_tex\standard_tex.frag.spv >> .\disasm\standard_tex\standard_tex.frag.spirv
-spirv-dis .\compiled\standard_tex\standard_tex.vert.spv >> .\disasm\standard_tex\standard_tex.vert.spirv
-spirv-dis .\compiled\imgui\imgui.frag.spv >> .\disasm\imgui\imgui.frag.spirv
-spirv-dis .\compiled\imgui\imgui.vert.spv >> .\disasm\imgui\imgui.vert.spirv
+spirv-dis .\compiled\standard\standard.frag.spv > .\disasm\standard\standard.frag.spirv
+spirv-dis .\compiled\standard\standard.vert.spv > .\disasm\standard\standard.vert.spirv
+spirv-dis .\compiled\standard_tex\standard_tex.frag.spv > .\disasm\standard_tex\standard_tex.frag.spirv
+spirv-dis .\compiled\standard_tex\standard_tex.vert.spv > .\disasm\standard_tex\standard_tex.vert.spirv
+spirv-dis .\compiled\imgui\imgui.frag.spv > .\disasm\imgui\imgui.frag.spirv
+spirv-dis .\compiled\imgui\imgui.vert.spv > .\disasm\imgui\imgui.vert.spirv
