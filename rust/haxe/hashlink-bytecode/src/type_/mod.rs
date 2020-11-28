3965241104b3e0f5ed47dc82e9f2ab4e68e941eb
Nathan Voglsam <nathanvoglsam420@gmail.com>
2020-11-28 18:40:46 +1100

    Add what I believe the various types in hashlink actually represent as documentation

--- rust/haxe/hashlink-bytecode/src/type_/mod.rs
+++ rust/haxe/hashlink-bytecode/src/type_/mod.rs
@@ -92,28 +92,101 @@ impl TypeKind {
 
 #[derive(Clone, Serialize, Deserialize)]
 pub enum Type {
-    // Primitive types
+    /// Void represents a *none* type. A function that returns void returns nothing, etc.
     Void,
+
+    /// 8 bit unsigned integer
     UI8,
+
+    /// 16 bit unsigned integer
     UI16,
+
+    /// 32 bit signed integer
     I32,
+
+    /// 64 bit signed integer
     I64,
+
+    /// 32 bit single-precision floating point number
     F32,
+
+    /// 64 bit double-precision floating point number
     F64,
+
+    /// A abstract boolean type with a platform defined size
     Bool,
+
+    /// A plain, untyped pointer that can be null
     Bytes,
+
+    /// Represents a boxed, dynamically typed object. This could also be thought of as an `Any`
+    /// type.
+    ///
+    /// This is essentially a pointer to a dynamically allocated block of memory that that contains
+    /// a type value pair. The type describes the runtime type of the value, and the value is the
+    /// value itself.
     Dynamic,
+
+    /// If `Bytes` is a pointer, `Array` is a slice. Array is a "fat pointer", which is just a tuple
+    /// of the length of the array with the pointer to the array's memory.
+    ///
+    /// This is also a value type as it is fundamentally still a pointer, it just carries a little
+    /// extra data with it.
     Array,
+
+    /// This type represents a value that holds a type identifier
     Type,
+
+    /// This represents a pointer to a dynamically typed object. This type also has the semantics of
+    /// a pointer. `DynObject` is similar to an object but is closer to what JavaScript or Lua would
+    /// call an object where all fields are dynamic to add and remove. This can be sort of thought
+    /// of as a hash-table (as that's basically what JS objects are) mapping a name to a value. The
+    /// value could be any type (primitive, an object, a closure)
     DynObject,
+
+    /// Represents a static function paired with its signature
     Function(TypeFunction),
+
+    /// Represents a member function for an object paired with its signature
     Method(TypeFunction),
+
+    /// This type wraps another underlying type to apply reference semantics to it. This would
+    /// primarily have the use case of passing references to value types through function arguments.
+    ///
+    /// A register of this type would have the semantics of a pointer
     Ref(TypeParam),
+
+    /// `Null` wraps an underlying type to make that underlying type capable of being nullable, if
+    /// it otherwise wouldn't be.
+    ///
+    /// The semantics of the type *DO NOT CHANGE* when wrapped by this, other than the value now
+    /// being able to represent `null`. An integer will remain a value type, will continue to be
+    /// passed by copying into functions, etc.
+    ///
+    /// The primary use case for this is optional values, like optional members or optional function
+    /// arguments.
     Null(TypeParam),
+
+    /// Represents a regular object, paired with its definition. A register of this type has the
+    /// semantics of a pointer
     Obj(TypeObject),
+
+    /// Represents an anonymous struct, paired with its definition. A register of this type has the
+    /// semantics of a pointer
     Struct(TypeObject),
+
     Virtual(TypeVirtual),
+
+    /// This represents an object type with a definition external to the compiled HashLink module.
+    /// This is a tool for allowing an object's memory layout to be opaque and defined by the host
+    /// application. It's a tool for integrating with native code.
     Abstract(TypeAbstract),
+
+    /// Represents an enum type. An enum type is a type which represents a collection of "variants"
+    /// that the type can contain.
+    ///
+    /// An enum is a *value* type and *NOT* a pointer. The whole structure is passed through
+    /// functions by *value*.
     Enum(TypeEnum),
 }
 
