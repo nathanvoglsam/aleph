21b9b2aaa915e42d0129624483ee07f67f895dbc
Nathan Voglsam <nathanvoglsam420@gmail.com>
2021-01-03 16:34:39 +1100

    fmt

--- rust/haxe/eon-hashlink-translation/src/translation/function/mod.rs
+++ rust/haxe/eon-hashlink-translation/src/translation/function/mod.rs
@@ -47,32 +47,34 @@ impl<'module, 'callable_table> FunctionTranslator<'module, 'callable_table> {
     }
 
     pub fn translate_function(&self, mut source: hashlink::Function) -> TranspileResult<Function> {
-        // This is a very, very, very, very hacky thing we inject into the instruction stream of every
-        // function we translate.
+        // This is a very, very, very, very hacky thing we inject into the instruction stream of
+        // every function we translate.
         //
-        // An unconditional jump with an offset of 0 is a no-op as the execution semantics are exactly
-        // equal to OpNop. This little hack serves one purpose, it guarantees that every function begins
-        // with a basic block with no predecessors and ends with an unconditional jump.
+        // An unconditional jump with an offset of 0 is a no-op as the execution semantics are
+        // exactly equal to OpNop. This little hack serves one purpose, it guarantees that every
+        // function begins with a basic block with no predecessors and ends with an unconditional
+        // jump.
         //
         // The purpose of this hack is to simplify our "mem2reg" implementation when dealing with
-        // function arguments. HashLink implicitly uses the 0..n registers for 0..n function arguments.
-        // There is no sane way to represent this kind of assignment semantics in the SSA graph we
-        // calculate in the next step without forcing the entire algorithm to work around this one edge
-        // case.
+        // function arguments. HashLink implicitly uses the 0..n registers for 0..n function
+        // arguments. There is no sane way to represent this kind of assignment semantics in the SSA
+        // graph we calculate in the next step without forcing the entire algorithm to work around
+        // this one edge case.
         //
-        // The problem stems from the fact that it is invalid, in Eon, for the *entry* basic block to be
-        // the target of a branch, as we can't encode phi instructions that import the values from the
-        // function arguments without introducing more painful edge cases to the instruction encoding.
-        // HashLink bytecode makes no guarantee, and actively has code generated, that violates this
-        // guarantee of Eon bytecode (if we translated directly without this hack anyway).
+        // The problem stems from the fact that it is invalid, in Eon, for the *entry* basic block
+        // to be the target of a branch, as we can't encode phi instructions that import the values
+        // from the function arguments without introducing more painful edge cases to the
+        // instruction encoding. HashLink bytecode makes no guarantee, and actively has code
+        // generated, that violates this guarantee of Eon bytecode (if we translated directly
+        // without this hack anyway).
         //
-        // The solution, just add a no-op branch at the start of the function. This, essentially, just
-        // explicitly encodes the edge of the execution graph that was otherwise only implicitly
-        // represented. There is an implicit execution graph edge from the caller into the callee, which
-        // is the execution edge that the function argument's SSA values are imported from. This empty
-        // basic block will encode that edge explicitly so we don't need to handle any edge cases when
-        // emitting phi instructions for branch target blocks (there's no sane way to handle having the
-        // first instruction be a branch target otherwise).
+        // The solution, just add a no-op branch at the start of the function. This, essentially,
+        // just explicitly encodes the edge of the execution graph that was otherwise only
+        // implicitly represented. There is an implicit execution graph edge from the caller into
+        // the callee, which is the execution edge that the function argument's SSA values are
+        // imported from. This empty basic block will encode that edge explicitly so we don't need
+        // to handle any edge cases when emitting phi instructions for branch target blocks (there's
+        // no sane way to handle having the first instruction be a branch target otherwise).
         let noop_jump = hashlink::OpOneParam { param_1: 0 };
         let noop_jump = hashlink::OpCode::OpJAlways(noop_jump);
         source.ops.insert(0, noop_jump);
