68bc24c8fc53241447033a56a2abfd1ee60aaa6f
Nathan Voglsam <nathanvoglsam420@gmail.com>
2021-02-08 20:44:01 +1100

    compile shaders to dxil not spirv

--- rust/engine/aleph-embedded-data/shaders/compile.bat
+++ rust/engine/aleph-embedded-data/shaders/compile.bat
@@ -1,9 +1,9 @@
 @echo off
-dxc /T ps_6_0 -Fo .\compiled\standard\standard.frag.spv -spirv .\source\standard\standard.frag.hlsl -O3 -I .\include -I .\source
-dxc /T vs_6_0 -Fo .\compiled\standard\standard.vert.spv -spirv .\source\standard\standard.vert.hlsl -O3 -I .\include -I .\source
-dxc /T ps_6_0 -Fo .\compiled\standard_tex\standard_tex.frag.spv -spirv .\source\standard_tex\standard_tex.frag.hlsl -O3 -I .\include -I .\source
-dxc /T vs_6_0 -Fo .\compiled\standard_tex\standard_tex.vert.spv -spirv .\source\standard_tex\standard_tex.vert.hlsl -O3 -I .\include -I .\source
-dxc /T ps_6_0 -Fo .\compiled\egui\egui.frag.spv -spirv .\source\egui\egui.frag.hlsl -O3 -I .\include -I .\source
-dxc /T vs_6_0 -Fo .\compiled\egui\egui.vert.spv -spirv .\source\egui\egui.vert.hlsl -O3 -I .\include -I .\source
-dxc /T ps_6_0 -Fo .\compiled\postprocess\tonemapping.frag.spv -spirv .\source\postprocess\tonemapping.frag.hlsl -O3 -I .\include -I .\source
-dxc /T vs_6_0 -Fo .\compiled\fullscreen_quad\fullscreen_quad.vert.spv -spirv .\source\fullscreen_quad\fullscreen_quad.vert.hlsl -O3 -I .\include -I .\source
+dxc /T ps_6_0 -Fo .\compiled\standard\standard.frag.dxil .\source\standard\standard.frag.hlsl -O3 -I .\include -I .\source -Wno-ignored-attributes
+dxc /T vs_6_0 -Fo .\compiled\standard\standard.vert.dxil .\source\standard\standard.vert.hlsl -O3 -I .\include -I .\source -Wno-ignored-attributes
+dxc /T ps_6_0 -Fo .\compiled\standard_tex\standard_tex.frag.dxil .\source\standard_tex\standard_tex.frag.hlsl -O3 -I .\include -I .\source -Wno-ignored-attributes
+dxc /T vs_6_0 -Fo .\compiled\standard_tex\standard_tex.vert.dxil .\source\standard_tex\standard_tex.vert.hlsl -O3 -I .\include -I .\source -Wno-ignored-attributes
+dxc /T ps_6_0 -Fo .\compiled\egui\egui.frag.dxil .\source\egui\egui.frag.hlsl -O3 -I .\include -I .\source -Wno-ignored-attributes
+dxc /T vs_6_0 -Fo .\compiled\egui\egui.vert.dxil .\source\egui\egui.vert.hlsl -O3 -I .\include -I .\source -Wno-ignored-attributes
+dxc /T ps_6_0 -Fo .\compiled\postprocess\tonemapping.frag.dxil .\source\postprocess\tonemapping.frag.hlsl -O3 -I .\include -I .\source -Wno-ignored-attributes
+dxc /T vs_6_0 -Fo .\compiled\fullscreen_quad\fullscreen_quad.vert.dxil .\source\fullscreen_quad\fullscreen_quad.vert.hlsl -O3 -I .\include -I .\source -Wno-ignored-attributes
