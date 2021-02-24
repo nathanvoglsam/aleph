41e6070447bb7650bbc40cd1667a73535a2c3982
Nathan Voglsam <nathanvoglsam420@gmail.com>
2021-02-25 00:52:03 +1100

    add map and unmap for resource

--- rust/graphics/dx12/aleph-dx12/src/dx12/resource.rs
+++ rust/graphics/dx12/aleph-dx12/src/dx12/resource.rs
@@ -27,13 +27,58 @@
 // SOFTWARE.
 //
 
-use crate::raw::windows::win32::direct3d12::ID3D12Resource;
+use crate::raw::windows::win32::direct3d12::{ID3D12Resource, D3D12_RANGE};
+use std::ops::Range;
+use std::ptr::NonNull;
 
 #[derive(Clone)]
 #[repr(transparent)]
 pub struct Resource(pub(crate) ID3D12Resource);
 
-impl Resource {}
+impl Resource {
+    pub fn get_gpu_virtual_address(&self) -> u64 {
+        unsafe { self.0.GetGPUVirtualAddress() }
+    }
+
+    pub fn map(
+        &self,
+        subresource: u32,
+        read_range: Option<Range<usize>>,
+    ) -> raw::windows::Result<Option<NonNull<u8>>> {
+        unsafe {
+            let mut out = std::ptr::null_mut();
+            if let Some(read_range) = read_range {
+                let read_range = D3D12_RANGE {
+                    begin: read_range.start,
+                    end: read_range.end,
+                };
+                self.0
+                    .Map(subresource, &read_range, &mut out)
+                    .ok()
+                    .map(|_| NonNull::new(out as *mut u8))
+            } else {
+                self.0
+                    .Map(subresource, std::ptr::null(), &mut out)
+                    .ok()
+                    .map(|_| NonNull::new(out as *mut u8))
+            }
+        }
+    }
+
+    pub fn unmap(&self, subresource: u32, written_range: Option<Range<usize>>) {
+        unsafe {
+            if let Some(written_range) = written_range {
+                let written_range = D3D12_RANGE {
+                    begin: written_range.start,
+                    end: written_range.end,
+                };
+                self.0.Unmap(subresource, &written_range)
+            } else {
+                self.0.Unmap(subresource, std::ptr::null())
+            }
+        }
+    }
+}
 
 crate::object_impl!(Resource);
 crate::device_child_impl!(Resource);
