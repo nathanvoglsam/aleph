994f0d0f61832dad6c2759b3faaa271d152d3733
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2021-10-09 19:17:54 +1100

    [aleph-dx12] Add utility constants and functions to dx12::ResourceStates

--- crates/aleph-dx12/src/dx12/resource_states.rs
+++ crates/aleph-dx12/src/dx12/resource_states.rs
@@ -34,32 +34,123 @@ use windows_raw::Win32::Direct3D12::D3D12_RESOURCE_STATES;
 pub struct ResourceStates(pub i32);
 
 impl ResourceStates {
-    pub const COMMON: Self = Self(0i32);
-    pub const VERTEX_AND_CONSTANT_BUFFER: Self = Self(1i32);
-    pub const INDEX_BUFFER: Self = Self(2i32);
-    pub const RENDER_TARGET: Self = Self(4i32);
-    pub const UNORDERED_ACCESS: Self = Self(8i32);
-    pub const DEPTH_WRITE: Self = Self(16i32);
-    pub const DEPTH_READ: Self = Self(32i32);
-    pub const NON_PIXEL_SHADER_RESOURCE: Self = Self(64i32);
-    pub const PIXEL_SHADER_RESOURCE: Self = Self(128i32);
-    pub const STREAM_OUT: Self = Self(256i32);
-    pub const INDIRECT_ARGUMENT: Self = Self(512i32);
-    pub const COPY_DEST: Self = Self(1024i32);
-    pub const COPY_SOURCE: Self = Self(2048i32);
-    pub const RESOLVE_DEST: Self = Self(4096i32);
-    pub const RESOLVE_SOURCE: Self = Self(8192i32);
-    pub const RAYTRACING_ACCELERATION_STRUCTURE: Self = Self(4194304i32);
-    pub const SHADING_RATE_SOURCE: Self = Self(16777216i32);
-    pub const GENERIC_READ: Self = Self(2755i32);
-    pub const PRESENT: Self = Self(0i32);
-    pub const PREDICATION: Self = Self(512i32);
-    pub const VIDEO_DECODE_READ: Self = Self(65536i32);
-    pub const VIDEO_DECODE_WRITE: Self = Self(131072i32);
-    pub const VIDEO_PROCESS_READ: Self = Self(262144i32);
-    pub const VIDEO_PROCESS_WRITE: Self = Self(524288i32);
-    pub const VIDEO_ENCODE_READ: Self = Self(2097152i32);
-    pub const VIDEO_ENCODE_WRITE: Self = Self(8388608i32);
+    // ----
+    // PURE FLAGS
+    // ----
+
+    pub const COMMON: Self = Self(0);
+    pub const VERTEX_AND_CONSTANT_BUFFER: Self = Self(0x1);
+    pub const INDEX_BUFFER: Self = Self(0x2);
+    pub const RENDER_TARGET: Self = Self(0x4);
+    pub const UNORDERED_ACCESS: Self = Self(0x8);
+    pub const DEPTH_WRITE: Self = Self(0x10);
+    pub const DEPTH_READ: Self = Self(0x20);
+    pub const NON_PIXEL_SHADER_RESOURCE: Self = Self(0x40);
+    pub const PIXEL_SHADER_RESOURCE: Self = Self(0x80);
+    pub const STREAM_OUT: Self = Self(0x100);
+    pub const INDIRECT_ARGUMENT: Self = Self(0x200);
+    pub const COPY_DEST: Self = Self(0x400);
+    pub const COPY_SOURCE: Self = Self(0x800);
+    pub const RESOLVE_DEST: Self = Self(0x1000);
+    pub const RESOLVE_SOURCE: Self = Self(0x2000);
+    pub const RAYTRACING_ACCELERATION_STRUCTURE: Self = Self(0x400000);
+    pub const SHADING_RATE_SOURCE: Self = Self(0x1000000);
+    pub const VIDEO_DECODE_READ: Self = Self(0x10000);
+    pub const VIDEO_DECODE_WRITE: Self = Self(0x20000);
+    pub const VIDEO_PROCESS_READ: Self = Self(0x40000);
+    pub const VIDEO_PROCESS_WRITE: Self = Self(0x80000);
+    pub const VIDEO_ENCODE_READ: Self = Self(0x200000);
+    pub const VIDEO_ENCODE_WRITE: Self = Self(0x800000);
+
+    // ----
+    // ALIAS FLAGS
+    // ----
+
+    pub const PRESENT: Self = Self::COMMON;
+    pub const PREDICATION: Self = Self::INDIRECT_ARGUMENT;
+
+    // ----
+    // COMPOSITE FLAGS
+    // ----
+
+    pub const GENERIC_READ: Self = Self(
+        Self::VERTEX_AND_CONSTANT_BUFFER.0
+            | Self::INDEX_BUFFER.0
+            | Self::NON_PIXEL_SHADER_RESOURCE.0
+            | Self::PIXEL_SHADER_RESOURCE.0
+            | Self::INDIRECT_ARGUMENT.0
+            | Self::COPY_SOURCE.0,
+    );
+    pub const ALL_SHADER_RESOURCE: Self =
+        Self(Self::NON_PIXEL_SHADER_RESOURCE.0 | Self::PIXEL_SHADER_RESOURCE.0);
+
+    pub const ALL_READ_ONLY: Self = Self(
+        Self::VERTEX_AND_CONSTANT_BUFFER.0
+            | Self::INDEX_BUFFER.0
+            | Self::DEPTH_READ.0
+            | Self::NON_PIXEL_SHADER_RESOURCE.0
+            | Self::PIXEL_SHADER_RESOURCE.0
+            | Self::INDIRECT_ARGUMENT.0
+            | Self::COPY_SOURCE.0
+            | Self::RESOLVE_SOURCE.0
+            | Self::SHADING_RATE_SOURCE.0
+            | Self::VIDEO_DECODE_READ.0
+            | Self::VIDEO_ENCODE_READ.0
+            | Self::VIDEO_PROCESS_READ.0,
+    );
+
+    pub const ALL_WRITES_ONLY: Self = Self(
+        Self::RENDER_TARGET.0
+            | Self::STREAM_OUT.0
+            | Self::COPY_DEST.0
+            | Self::RESOLVE_DEST.0
+            | Self::DEPTH_WRITE.0
+            | Self::VIDEO_DECODE_WRITE.0
+            | Self::VIDEO_ENCODE_WRITE.0
+            | Self::VIDEO_PROCESS_WRITE.0,
+    );
+
+    pub const ALL_READ_WRITES: Self =
+        Self(Self::UNORDERED_ACCESS.0 | Self::RAYTRACING_ACCELERATION_STRUCTURE.0);
+}
+
+impl ResourceStates {
+    /// Returns whether the state of `self` represent a purely read-only resource state.
+    #[inline]
+    pub fn is_read_only(&self) -> bool {
+        self.is_subset_of(&Self::ALL_READ_ONLY)
+    }
+
+    /// Returns whether the state of `self` would allow read access to the resource. This does not
+    /// mean the access is read-only, use [`ResourceStates::is_read_only`] to detect read-only
+    /// states.
+    #[inline]
+    pub fn is_read(&self) -> bool {
+        self.intersects(&Self::ALL_READ_ONLY) || self.intersects(&Self::ALL_READ_WRITES)
+    }
+
+    /// Returns whether the state of `self` allows both read and write access to the resource. This
+    /// will check if the state is composed of a combination of read-only states and write-only
+    /// states or if the state is one of the read-write states (i.e UNORDERED_ACCESS).
+    #[inline]
+    pub fn is_read_write(&self) -> bool {
+        self.intersects(&Self::ALL_READ_WRITES)
+            || (self.intersects(&Self::ALL_WRITES_ONLY) && self.intersects(&Self::ALL_READ_ONLY))
+    }
+
+    /// Returns whether the state of `self` represent a purely write-only resource state.
+    #[inline]
+    pub fn is_write_only(&self) -> bool {
+        self.is_subset_of(&Self::ALL_WRITES_ONLY)
+    }
+
+    /// Returns whether the state of `self` would allow write access to the resource. This does not
+    /// mean the access is write-only, use [`ResourceStates::is_write_only`] to detect write-only
+    /// states.
+    #[inline]
+    pub fn is_write(&self) -> bool {
+        self.intersects(&Self::ALL_WRITES_ONLY) || self.intersects(&Self::ALL_READ_WRITES)
+    }
 }
 
 impl Default for ResourceStates {
