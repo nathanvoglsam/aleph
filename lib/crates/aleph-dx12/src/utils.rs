7da2eae04c7780d448455ea701d82647b313d961
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2022-01-26 18:19:04 +1100

    [aleph-dx12] Add wrapper for COM weak references

--- lib/crates/aleph-dx12/src/utils.rs
+++ lib/crates/aleph-dx12/src/utils.rs
@@ -27,6 +27,67 @@
 // SOFTWARE.
 //
 
+use std::marker::PhantomData;
+use std::mem::ManuallyDrop;
+use std::ops::Deref;
+
+///
+/// A wrapper type that allows sharing weak references to COM interfaces. This allows sharing
+/// 'pseudo-owned' copies of a COM interface without incrementing the reference counter. The
+/// lifetime invariant is upheld by a phantom borrow.
+///
+#[repr(transparent)]
+#[derive(Clone)]
+pub struct WeakRef<'a, T> {
+    pub(crate) v: ManuallyDrop<T>,
+    pub(crate) phantom: PhantomData<&'a T>,
+}
+
+impl<'a, T: Clone> Deref for WeakRef<'a, T> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.v.deref()
+    }
+}
+
+///
+/// Trait implemented on all COM wrapper types that allows producing [WeakRef] handles to the
+/// underlying COM interface
+///
+/// # Safety
+///
+/// This trait only really makes sense when implemented on our COM object wrappers so we mark it as
+/// unsafe. The implementations of this trait will almost always require unsafe code anyway.
+///
+/// Generally this will require making a copy of the object without using [Clone] and so it will
+/// almost certainly be UB.
+///
+/// For COM stuff it isn't UB as the wrappers are just transparent wrappers around a single pointer.
+/// They're perfectly safe to do copies without [Clone] as long as we make sure that the reference
+/// count is incremented safely.
+///
+pub unsafe trait AsWeakRef: Sized {
+    fn as_weak(&self) -> WeakRef<Self>;
+}
+
+#[macro_export]
+macro_rules! as_weak_ref_impl {
+    ($t:ident) => {
+        unsafe impl $crate::utils::AsWeakRef for $t {
+            fn as_weak(&self) -> $crate::utils::WeakRef<Self> {
+                unsafe {
+                    $crate::utils::WeakRef {
+                        v: core::mem::ManuallyDrop::new(core::mem::transmute_copy::<$t, $t>(self)),
+                        phantom: core::default::Default::default(),
+                    }
+                }
+            }
+        }
+    };
+}
+
 #[macro_export]
 macro_rules! device_child_impl {
     ($t:ident) => {
