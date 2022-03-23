dd0ebb8778949200f0b7a5700b36dac0a9721554
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2022-03-23 21:41:14 +1100

    [aleph-gpu-dx12] Update SwapChain

--- lib/plugins/aleph-gpu-dx12/src/swap_chain.rs
+++ lib/plugins/aleph-gpu-dx12/src/swap_chain.rs
@@ -27,17 +27,79 @@
 // SOFTWARE.
 //
 
+use crate::device::Device;
 use crate::surface::Surface;
+use crate::swap_texture::SwapTexture;
+use crossbeam::atomic::AtomicCell;
 use dx12::dxgi;
-use interfaces::gpu::{AcquireImageError, ISwapChain, QueueType};
-use interfaces::ref_ptr::{ref_ptr_object, RefPtr};
+use interfaces::gpu::{
+    AcquireImageError, INamedObject, ISwapChain, ITexture, QueueType, ResourceStates,
+    SwapChainConfiguration, TextureDesc, TextureDimension,
+};
+use interfaces::ref_ptr::{ref_ptr_init, ref_ptr_object, RefPtr, RefPtrObject};
+use parking_lot::Mutex;
+use std::ops::Deref;
 use std::sync::atomic::Ordering;
 
 ref_ptr_object! {
     pub struct SwapChain: ISwapChain, ISwapChainExt {
         pub(crate) swap_chain: dxgi::SwapChain,
+        pub(crate) device: RefPtr<Device>,
         pub(crate) surface: RefPtr<Surface>,
         pub(crate) queue_support: QueueType,
+        pub(crate) inner: Mutex<SwapChainState>,
+        pub(crate) queued_resize: AtomicCell<Option<Box<(u32, u32)>>>,
+    }
+}
+
+pub struct SwapChainState {
+    pub config: SwapChainConfiguration,
+    pub acquired: bool,
+    pub images: Vec<(dx12::Resource, dx12::CPUDescriptorHandle)>,
+    pub dxgi_format: dxgi::Format,
+    pub dxgi_view_format: dxgi::Format,
+}
+
+impl SwapChain {
+    pub unsafe fn handle_resize(&self, inner: &mut SwapChainState, width: u32, height: u32) {
+        let queue = match self.queue_support {
+            QueueType::General => self.device.queues.general.as_ref().unwrap().lock().clone(),
+            QueueType::Compute => self.device.queues.compute.as_ref().unwrap().lock().clone(),
+            QueueType::Transfer => self.device.queues.transfer.as_ref().unwrap().lock().clone(),
+        };
+        let queues: Vec<dx12::CommandQueue> = inner
+            .images
+            .drain(..)
+            .map(|(_, view)| {
+                self.device.rtv_heap.free(view);
+                queue.clone()
+            })
+            .collect();
+
+        self.swap_chain
+            .resize_buffers(
+                queues.len() as u32,
+                width,
+                height,
+                dxgi::Format::Unknown,
+                dxgi::SwapChainFlags::NONE,
+                None,
+                &queues,
+            )
+            .unwrap();
+
+        let images = self
+            .device
+            .create_views_for_swap_images(
+                &self.swap_chain,
+                inner.dxgi_view_format,
+                queues.len() as u32,
+            )
+            .unwrap();
+
+        inner.images = images;
+        inner.config.width = width;
+        inner.config.height = height;
     }
 }
 
@@ -46,8 +108,62 @@ impl ISwapChain for SwapChain {
         queue == self.queue_support
     }
 
-    fn acquire_image(&self) -> Result<(), AcquireImageError> {
-        todo!()
+    fn queue_resize(&self, width: u32, height: u32) {
+        let resize = Box::new((width, height));
+        self.queued_resize.store(Some(resize));
+    }
+
+    fn get_config(&self) -> SwapChainConfiguration {
+        self.inner.lock().config.clone()
+    }
+
+    fn acquire_image(&self) -> Result<RefPtr<dyn ITexture>, AcquireImageError> {
+        let mut inner = self.inner.lock();
+
+        if inner.acquired {
+            return Err(AcquireImageError::ImageNotAvailable);
+        }
+
+        if let Some(dimensions) = self.queued_resize.take() {
+            // TODO: We need to ensure that the swap chain is no longer in use before we do any of
+            //       this. We also need to make sure all dx12::Resource handles to the swap images
+            //       have been dropped.
+            let new_width = dimensions.deref().0;
+            let new_height = dimensions.deref().1;
+            unsafe {
+                self.handle_resize(&mut inner, new_width, new_height);
+            }
+        }
+
+        let image = self.swap_chain.get_current_back_buffer_index();
+        let image = ref_ptr_init! {
+            SwapTexture {
+                resource: inner.images[image as usize].0.clone(),
+                view: inner.images[image as usize].1,
+                desc: TextureDesc {
+                    width: inner.config.width,
+                    height: inner.config.height,
+                    depth: 1,
+                    format: inner.config.format,
+                    dimension: TextureDimension::Texture2D,
+                    initial_state: ResourceStates::PRESENT,
+                    clear_value: None,
+                    array_size: 1,
+                    mip_levels: 1,
+                    sample_count: 1,
+                    sample_quality: 0,
+                    allow_unordered_access: false,
+                    allow_cube_face: false,
+                    is_render_target: true
+                },
+                swap_chain: self.as_ref_ptr(),
+            }
+        };
+        let image: RefPtr<SwapTexture> = RefPtr::new(image);
+
+        inner.acquired = true;
+
+        Ok(image.query_interface().unwrap())
     }
 }
 
@@ -56,17 +172,39 @@ impl Drop for SwapChain {
         // Release the surface as the swap chain no longer owns it
         debug_assert!(self.surface.has_swap_chain.swap(false, Ordering::SeqCst));
 
-        // TODO: We will need to manually extend the lifetime of the SwapChain so we can perform the
-        //       above operation when we know that the swap chain is no longer in use
+        let mut inner = self.inner.lock();
+        for (_, view) in inner.images.drain(..) {
+            self.device.rtv_heap.free(view);
+        }
+        assert!(!inner.acquired);
     }
 }
 
 pub trait ISwapChainExt: ISwapChain {
-    fn get_raw_handle(&self) -> &dxgi::SwapChain;
+    fn get_raw_handle(&self) -> dxgi::SwapChain;
+
+    fn get_raw_in_memory_format(&self) -> dxgi::Format;
+
+    fn get_raw_view_format(&self) -> dxgi::Format;
 }
 
 impl ISwapChainExt for SwapChain {
-    fn get_raw_handle(&self) -> &dxgi::SwapChain {
-        &self.swap_chain
+    fn get_raw_handle(&self) -> dxgi::SwapChain {
+        self.swap_chain.clone()
+    }
+
+    fn get_raw_in_memory_format(&self) -> dxgi::Format {
+        self.inner.lock().dxgi_format
+    }
+
+    fn get_raw_view_format(&self) -> dxgi::Format {
+        self.inner.lock().dxgi_view_format
+    }
+}
+
+impl INamedObject for SwapChain {
+    fn set_name(&self, _name: &str) {
+        // Nothing to do on d3d12 as swap chains can't be named. SwapChain comes from dxgi which
+        // doesn't implement D3D12Object.
     }
 }
