272016cff42c20389f2bf57923aa970956b44303
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2022-03-24 23:46:56 +1100

    [aleph-gpu-dx12] Implement texture clears for RTV and DSV

--- lib/plugins/aleph-gpu-dx12/src/encoder.rs
+++ lib/plugins/aleph-gpu-dx12/src/encoder.rs
@@ -52,32 +52,231 @@ impl<'a> Drop for Encoder<'a> {
     }
 }
 
+impl<'a> Encoder<'a> {
+    #[inline]
+    fn clear_swap_texture(
+        &mut self,
+        texture: WeakRefPtr<dyn ITexture>,
+        concrete: WeakRefPtr<SwapTexture>,
+        value: &ColorClearValue,
+    ) {
+        let buffer = match value {
+            ColorClearValue::Float { r, g, b, a } => [*r, *g, *b, *a],
+            ColorClearValue::Int(v) => decode_u32_color_to_float(*v),
+        };
+
+        unsafe {
+            self.list
+                .clear_render_target_view(concrete.view, &buffer, None);
+        }
+        self.tracker.images.push(texture.to_strong());
+    }
+
+    #[inline]
+    fn clear_plain_texture(
+        &mut self,
+        texture: WeakRefPtr<dyn ITexture>,
+        concrete: WeakRefPtr<Texture>,
+        sub_resources: &TextureSubResourceSet,
+        value: &ColorClearValue,
+    ) {
+        if concrete.desc.format.is_depth_stencil() {
+            aleph_log::debug!("Tried to perform clear_color on a depth/stencil texture");
+            return;
+        }
+
+        let buffer = match value {
+            ColorClearValue::Float { r, g, b, a } => [*r, *g, *b, *a],
+            ColorClearValue::Int(v) => decode_u32_color_to_float(*v),
+        };
+
+        let sub_resources = self.clamp_sub_resource_set_to_texture(&concrete.desc, sub_resources);
+
+        // DX12 handles clearing textures differently between render targets and non render target
+        // textures.
+        if concrete.desc.is_render_target {
+            // DX12 can only clear a single mip level per call to ClearRenderTargetView, to clear
+            // all the requested layers we need to emit multiple calls to ClearRenderTargetView.
+            let begin = sub_resources.base_mip_level;
+            let end = begin + sub_resources.num_mip_levels;
+            for level in begin..end {
+                let level_sub_resources = TextureSubResourceSet {
+                    base_mip_level: level,
+                    num_mip_levels: 1,
+                    base_array_slice: sub_resources.base_array_slice,
+                    num_array_slices: sub_resources.base_mip_level,
+                };
+
+                let view = concrete.get_or_create_rtv_for_usage(None, &level_sub_resources);
+
+                if let Some(view) = view {
+                    unsafe {
+                        self.list.clear_render_target_view(view, &buffer, None);
+                    }
+                } else {
+                    aleph_log::debug!(
+                        "Called IEncoder::clear_texture with TextureSubResourceSet::num_mip_levels = 0."
+                    );
+                    return;
+                }
+            }
+        } else {
+            todo!()
+        }
+
+        self.tracker.images.push(texture.to_strong());
+    }
+
+    #[inline]
+    fn clear_depth_image(
+        &mut self,
+        texture: WeakRefPtr<dyn ITexture>,
+        concrete: WeakRefPtr<Texture>,
+        sub_resources: &TextureSubResourceSet,
+        value: &DepthStencilClearValue,
+    ) {
+        if !concrete.desc.format.is_depth_stencil() {
+            aleph_log::debug!("Tried to perform clear_depth_stencil_texture on a color texture");
+            return;
+        }
+
+        let (depth, stencil, clear_flags) = match value {
+            DepthStencilClearValue::DepthStencil(d, s) => (*d, *s, dx12::ClearFlags::all()),
+            DepthStencilClearValue::Depth(d) => (*d, 0, dx12::ClearFlags::DEPTH),
+            DepthStencilClearValue::Stencil(s) => (0.0, *s, dx12::ClearFlags::STENCIL),
+        };
+
+        let sub_resources = self.clamp_sub_resource_set_to_texture(&concrete.desc, sub_resources);
+
+        // DX12 handles clearing textures differently between render targets and non render target
+        // textures.
+        if concrete.desc.is_render_target {
+            // DX12 can only clear a single mip level per call to ClearRenderTargetView, to clear
+            // all the requested layers we need to emit multiple calls to ClearRenderTargetView.
+            let begin = sub_resources.base_mip_level;
+            let end = begin + sub_resources.num_mip_levels;
+            for level in begin..end {
+                let level_sub_resources = TextureSubResourceSet {
+                    base_mip_level: level,
+                    num_mip_levels: 1,
+                    base_array_slice: sub_resources.base_array_slice,
+                    num_array_slices: sub_resources.base_mip_level,
+                };
+
+                let view = concrete.get_or_create_dsv_for_usage(None, &level_sub_resources);
+                if let Some(view) = view {
+                    unsafe {
+                        self.list
+                            .clear_depth_stencil_view(view, clear_flags, depth, stencil, None);
+                    }
+                } else {
+                    aleph_log::debug!(
+                    "Called IEncoder::clear_depth_stencil_texture with TextureSubResourceSet::num_mip_levels = 0."
+                );
+                    return;
+                }
+            }
+        }
+
+        self.tracker.images.push(texture.to_strong());
+    }
+
+    #[inline]
+    fn clamp_sub_resource_set_to_texture(
+        &self,
+        texture: &TextureDesc,
+        sub_resources: &TextureSubResourceSet,
+    ) -> TextureSubResourceSet {
+        #[inline(always)]
+        fn clamp_range(base: u32, len: u32, min: u32, max: u32) -> (u32, u32) {
+            let base_level = base.clamp(min, max);
+            let end_level = base_level + len;
+            let end_level_clamped = end_level.clamp(min, max);
+            let num_levels = (end_level_clamped + 1) - base_level;
+            (base_level, num_levels)
+        }
+
+        let min_mip_level = 0;
+        let max_mip_level = texture.mip_levels - 1;
+        let (base_mip_level, num_mip_levels) = clamp_range(
+            sub_resources.base_mip_level,
+            sub_resources.num_mip_levels,
+            min_mip_level,
+            max_mip_level,
+        );
+
+        let min_array_slice = 0;
+        let max_array_slice = texture.array_size - 1;
+        let (base_array_slice, num_array_slices) = clamp_range(
+            sub_resources.base_array_slice,
+            sub_resources.num_array_slices,
+            min_array_slice,
+            max_array_slice,
+        );
+
+        // Warn if the base mip level is out of bounds
+        if sub_resources.base_mip_level >= max_mip_level {
+            aleph_log::debug!("TextureSubResourceSet out of bounds: base_mip_level");
+        }
+
+        // Warn if the sub resource set is addressing the mip levels out of bounds unintentionally
+        //
+        // u32::MAX flags "use all mip levels"
+        let use_all_mips = texture.mip_levels == u32::MAX;
+        if (base_mip_level + num_mip_levels) > max_mip_level && !use_all_mips {
+            aleph_log::debug!("TextureSubResourceSet out of bounds: num_mip_levels")
+        }
+
+        // Warn if the base array slice is out of bounds
+        if sub_resources.base_array_slice >= max_array_slice {
+            aleph_log::debug!("TextureSubResourceSet out of bounds: base_array_slice");
+        }
+
+        // Warn if the sub resource set is addressing the array slices out of bounds unintentionally
+        //
+        // u32::MAX flags "use all array slices"
+        let use_all_slices = texture.array_size == u32::MAX;
+        if (base_array_slice + num_array_slices) > max_array_slice && !use_all_slices {
+            aleph_log::debug!("TextureSubResourceSet out of bounds: num_array_slices")
+        }
+
+        TextureSubResourceSet {
+            base_mip_level,
+            num_mip_levels,
+            base_array_slice,
+            num_array_slices,
+        }
+    }
+}
+
 impl<'a> IGeneralEncoder for Encoder<'a> {
     fn clear_texture(
         &mut self,
         texture: WeakRefPtr<dyn ITexture>,
-        _subresources: &TextureSubresourceSet,
-        value: &ClearValue,
+        sub_resources: &TextureSubResourceSet,
+        value: &ColorClearValue,
     ) {
-        let _texture = texture.query_interface::<Texture>().unwrap();
-        match value {
-            ClearValue::ColorF32(v) => unsafe {
-                let buffer = [v.r, v.g, v.b, v.a];
-                self.list.clear_render_target_view(todo!(), &buffer, None);
-            },
-            ClearValue::ColorInt(v) => unsafe {
-                let buffer = decode_u32_color_to_float(*v);
-                self.list.clear_render_target_view(todo!(), &buffer, None);
-            },
-            ClearValue::DepthStencil(depth, stencil) => unsafe {
-                self.list.clear_depth_stencil_view(
-                    todo!(),
-                    dx12::ClearFlags::all(),
-                    *depth,
-                    *stencil,
-                    None,
-                );
-            },
+        if let Some(concrete) = texture.query_interface::<Texture>() {
+            self.clear_plain_texture(texture.clone(), concrete, sub_resources, value);
+        } else if let Some(concrete) = texture.query_interface::<SwapTexture>() {
+            self.clear_swap_texture(texture.clone(), concrete, value);
+        } else {
+            panic!("Unknown ITexture implementation");
+        }
+    }
+
+    fn clear_depth_stencil_texture(
+        &mut self,
+        texture: WeakRefPtr<dyn ITexture>,
+        sub_resources: &TextureSubResourceSet,
+        value: &DepthStencilClearValue,
+    ) {
+        if let Some(concrete) = texture.query_interface::<Texture>() {
+            self.clear_depth_image(texture.clone(), concrete, sub_resources, value);
+        } else if let Some(_) = texture.query_interface::<SwapTexture>() {
+            aleph_log::debug!("Tried to clear swap chain image as a depth stencil texture");
+        } else {
+            panic!("Unknown ITexture implementation");
         }
     }
 
--- lib/plugins/aleph-gpu-dx12/src/texture.rs
+++ lib/plugins/aleph-gpu-dx12/src/texture.rs
@@ -51,7 +51,239 @@ ref_ptr_object! {
     }
 }
 
-impl Texture {}
+impl Texture {
+    #[inline]
+    pub fn get_or_create_rtv_for_usage(
+        &self,
+        format: Option<TextureFormat>,
+        sub_resources: &TextureSubResourceSet,
+    ) -> Option<dx12::CPUDescriptorHandle> {
+        let init = |view, format, sub_resources| unsafe {
+            let desc = self.make_rtv_desc_for_format_and_sub_resources(format, &sub_resources);
+            self.device
+                .device
+                .create_render_target_view(&self.resource, &desc, view);
+        };
+        self.get_or_create_view_for_usage(
+            &self.rtv_cache,
+            &self.device.rtv_heap,
+            format,
+            sub_resources,
+            false,
+            init,
+        )
+    }
+
+    #[inline]
+    pub fn get_or_create_dsv_for_usage(
+        &self,
+        format: Option<TextureFormat>,
+        sub_resources: &TextureSubResourceSet,
+    ) -> Option<dx12::CPUDescriptorHandle> {
+        let init = |view, format, sub_resources| unsafe {
+            let desc = self.make_dsv_desc_for_format_and_sub_resources(format, &sub_resources);
+            self.device
+                .device
+                .create_depth_stencil_view(&self.resource, &desc, view);
+        };
+        self.get_or_create_view_for_usage(
+            &self.dsv_cache,
+            &self.device.dsv_heap,
+            format,
+            sub_resources,
+            false,
+            init,
+        )
+    }
+
+    #[inline]
+    pub fn get_or_create_view_for_usage(
+        &self,
+        cache: &RwLock<CacheViewCPU>,
+        allocator: &DescriptorAllocatorCPU,
+        format: Option<TextureFormat>,
+        sub_resources: &TextureSubResourceSet,
+        allow_multiple_mips: bool,
+        init: impl FnOnce(dx12::CPUDescriptorHandle, TextureFormat, TextureSubResourceSet),
+    ) -> Option<dx12::CPUDescriptorHandle> {
+        // First see if we already have a compatible view
+        //
+        // We intentionally take a read lock optimistically as very likely the view is already in
+        // the cache. If it isn't then we hit the slow path of initializing the view so we need to
+        // take a write lock.
+        let views = cache.read();
+
+        // Whether more than a single mip level is valid for this view
+        let sub_resources = if !allow_multiple_mips && sub_resources.num_mip_levels > 1 {
+            let mut sub_resources = sub_resources.clone();
+            sub_resources.num_mip_levels = 1;
+            sub_resources
+        } else {
+            sub_resources.clone()
+        };
+
+        // Zero mip levels would imply no image data so is also invalid
+        if sub_resources.num_mip_levels < 1 {
+            return None;
+        }
+
+        let format = format.unwrap_or(self.desc.format);
+        let key = (format.clone(), sub_resources.clone());
+        if let Some(view) = views.get(&key) {
+            Some(*view)
+        } else {
+            // Otherwise we need to create a new one. We drop the old read lock and take a new write
+            // lock so we can get exclusive access to the map
+            drop(views);
+            let mut views = cache.write();
+
+            // Allocate a descriptor and write the view into it
+            let view = allocator.allocate().unwrap();
+
+            // Call the initializer to write the descriptor
+            (init)(view, format, sub_resources);
+
+            // Add the view to our cache
+            views.insert(key, view);
+
+            Some(view)
+        }
+    }
+
+    #[inline]
+    pub fn make_rtv_desc_for_format_and_sub_resources(
+        &self,
+        format: TextureFormat,
+        sub_resources: &TextureSubResourceSet,
+    ) -> dx12::RenderTargetViewDesc {
+        let is_array = self.desc.array_size > 1;
+        let is_ms = self.desc.sample_count > 1;
+
+        match (self.desc.dimension, is_array, is_ms) {
+            (TextureDimension::Texture1D, true, _) => dx12::RenderTargetViewDesc::Texture1DArray {
+                format: texture_format_to_dxgi(format),
+                texture_1d_array: dx12::Tex1DArrayRtv {
+                    mip_slice: sub_resources.base_mip_level,
+                    first_array_slice: sub_resources.base_array_slice,
+                    array_size: sub_resources.num_array_slices,
+                },
+            },
+            (TextureDimension::Texture1D, false, _) => dx12::RenderTargetViewDesc::Texture1D {
+                format: texture_format_to_dxgi(format),
+                texture_1d: dx12::Tex1DRtv {
+                    mip_slice: sub_resources.base_mip_level,
+                },
+            },
+            (TextureDimension::Texture2D, true, false) => {
+                dx12::RenderTargetViewDesc::Texture2DArray {
+                    format: texture_format_to_dxgi(format),
+                    texture_2d_array: dx12::Tex2DArrayRtv {
+                        mip_slice: sub_resources.base_mip_level,
+                        first_array_slice: sub_resources.base_array_slice,
+                        array_size: sub_resources.num_array_slices,
+                        plane_slice: 0,
+                    },
+                }
+            }
+            (TextureDimension::Texture2D, false, false) => dx12::RenderTargetViewDesc::Texture2D {
+                format: texture_format_to_dxgi(format),
+                texture_2d: dx12::Tex2DRtv {
+                    mip_slice: sub_resources.base_mip_level,
+                    plane_slice: 0,
+                },
+            },
+            (TextureDimension::Texture2D, true, true) => {
+                dx12::RenderTargetViewDesc::Texture2DMSArray {
+                    format: texture_format_to_dxgi(format),
+                    texture_2dms_array: dx12::Tex2DMSArrayRtv {
+                        first_array_slice: sub_resources.base_array_slice,
+                        array_size: sub_resources.num_array_slices,
+                    },
+                }
+            }
+            (TextureDimension::Texture2D, false, true) => dx12::RenderTargetViewDesc::Texture2DMS {
+                format: texture_format_to_dxgi(format),
+                texture_2dms: dx12::Tex2DMSRtv { _unused: 0 },
+            },
+            (TextureDimension::Texture3D, _, _) => dx12::RenderTargetViewDesc::Texture3D {
+                format: texture_format_to_dxgi(format),
+                texture_3d: dx12::Tex3DRtv {
+                    mip_slice: sub_resources.base_mip_level,
+                    first_w_slice: sub_resources.base_array_slice,
+                    w_size: sub_resources.num_array_slices,
+                },
+            },
+        }
+    }
+
+    #[inline]
+    pub fn make_dsv_desc_for_format_and_sub_resources(
+        &self,
+        format: TextureFormat,
+        sub_resources: &TextureSubResourceSet,
+    ) -> dx12::DepthStencilViewDesc {
+        let is_array = self.desc.array_size > 1;
+        let is_ms = self.desc.sample_count > 1;
+
+        match (self.desc.dimension, is_array, is_ms) {
+            (TextureDimension::Texture1D, true, _) => dx12::DepthStencilViewDesc::Texture1DArray {
+                format: texture_format_to_dxgi(format),
+                texture_1d_array: dx12::Tex1DArrayDsv {
+                    mip_slice: sub_resources.base_mip_level,
+                    first_array_slice: sub_resources.base_array_slice,
+                    array_size: sub_resources.num_array_slices,
+                },
+            },
+            (TextureDimension::Texture1D, false, _) => dx12::DepthStencilViewDesc::Texture1D {
+                format: texture_format_to_dxgi(format),
+                texture_1d: dx12::Tex1DDsv {
+                    mip_slice: sub_resources.base_mip_level,
+                },
+            },
+            (TextureDimension::Texture2D, true, false) => {
+                dx12::DepthStencilViewDesc::Texture2DArray {
+                    format: texture_format_to_dxgi(format),
+                    texture_2d_array: dx12::Tex2DArrayDsv {
+                        mip_slice: sub_resources.base_mip_level,
+                        first_array_slice: sub_resources.base_array_slice,
+                        array_size: sub_resources.num_array_slices,
+                    },
+                }
+            }
+            (TextureDimension::Texture2D, false, false) => dx12::DepthStencilViewDesc::Texture2D {
+                format: texture_format_to_dxgi(format),
+                texture_2d: dx12::Tex2DDsv {
+                    mip_slice: sub_resources.base_mip_level,
+                },
+            },
+            (TextureDimension::Texture2D, true, true) => {
+                dx12::DepthStencilViewDesc::Texture2DMSArray {
+                    format: texture_format_to_dxgi(format),
+                    texture_2dms_array: dx12::Tex2DMSArrayDsv {
+                        first_array_slice: sub_resources.base_array_slice,
+                        array_size: sub_resources.num_array_slices,
+                    },
+                }
+            }
+            (TextureDimension::Texture2D, false, true) => dx12::DepthStencilViewDesc::Texture2DMS {
+                format: texture_format_to_dxgi(format),
+                texture_2dms: dx12::Tex2DMSDsv { _unused: 0 },
+            },
+            (TextureDimension::Texture3D, _, _) => unreachable!(),
+        }
+    }
+}
+
+impl Drop for Texture {
+    #[inline]
+    fn drop(&mut self) {
+        // Free all RTVs associated with this texture
+        let rtvs = self.rtv_cache.read();
+        for (_, rtv) in rtvs.iter() {
+            self.device.rtv_heap.free(*rtv);
+        }
+    }
+}
 
 impl ITexture for Texture {
     fn desc(&self) -> &TextureDesc {
