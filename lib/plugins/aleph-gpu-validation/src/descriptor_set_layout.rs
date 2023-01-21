28b3d2a4a78447c52e79e0284fd5b1982c260d9b
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2023-01-21 13:48:23 +1100

    [aleph-gpu-validation] Add descriptor binding metadata to ValidationDescriptorSetLayout

--- lib/plugins/aleph-gpu-validation/src/descriptor_set_layout.rs
+++ lib/plugins/aleph-gpu-validation/src/descriptor_set_layout.rs
@@ -29,15 +29,27 @@
 
 use crate::device::ValidationDevice;
 use interfaces::any::{AnyArc, AnyWeak};
-use interfaces::gpu::{IDescriptorSetLayout, INamedObject};
+use interfaces::gpu::{DescriptorType, IDescriptorSetLayout, INamedObject};
+use std::collections::HashMap;
+use std::num::NonZeroU32;
 
 pub struct ValidationDescriptorSetLayout {
     pub(crate) _this: AnyWeak<Self>,
     pub(crate) _device: AnyArc<ValidationDevice>,
     pub(crate) inner: AnyArc<dyn IDescriptorSetLayout>,
+    pub(crate) binding_info: HashMap<u32, DescriptorBindingInfo>,
 }
 
 crate::validation_declare_interfaces!(ValidationDescriptorSetLayout, [IDescriptorSetLayout]);
+
+impl ValidationDescriptorSetLayout {
+    /// Internal function
+    ///
+    /// Queries the binding metadata for the given binding index.
+    pub fn get_binding_info(&self, binding: u32) -> Option<DescriptorBindingInfo> {
+        self.binding_info.get(&binding).cloned()
+    }
+}
 
 impl IDescriptorSetLayout for ValidationDescriptorSetLayout {
     fn upgrade(&self) -> AnyArc<dyn IDescriptorSetLayout> {
@@ -57,4 +69,21 @@ impl INamedObject for ValidationDescriptorSetLayout {
     fn set_name(&self, name: &str) {
         self.inner.set_name(name)
     }
+}
+
+/// Internal struct, stores computed cached info about a descriptor binding
+#[derive(Clone)]
+pub struct DescriptorBindingInfo {
+    /// The type of descriptor this binding contains
+    pub r#type: DescriptorType,
+
+    /// The number of descriptors in the binding. [None] encodes a non-array binding with 1
+    /// descriptor, while [Some(n)] encodes an array binding with 'n' descriptors.
+    pub descriptor_count: Option<NonZeroU32>,
+
+    /// Whether this binding is a static sampler.
+    pub is_static_sampler: bool,
+
+    /// Declares whether the descriptor's underlying resource can be accessed with write access.
+    pub allow_writes: bool,
 }
