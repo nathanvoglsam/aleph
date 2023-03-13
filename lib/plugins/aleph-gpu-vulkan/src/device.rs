b1e15d9544d8cb43fa249e5b1f95e491683e9c30
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2023-03-13 21:46:11 +1100

    [aleph-gpu-vulkan] Big reorganization of pipeline creation code

--- lib/plugins/aleph-gpu-vulkan/src/device.rs
+++ lib/plugins/aleph-gpu-vulkan/src/device.rs
@@ -30,7 +30,11 @@
 use crate::adapter::Adapter;
 use crate::context::Context;
 use crate::fence::Fence;
-use crate::internal::conv::texture_format_to_vk;
+use crate::internal::conv::{
+    blend_factor_to_vk, blend_op_to_vk, compare_op_to_vk, cull_mode_to_vk, front_face_order_to_vk,
+    polygon_mode_to_vk, primitive_topology_to_vk, stencil_op_to_vk, texture_format_to_vk,
+    vertex_input_rate_to_vk,
+};
 use crate::internal::queues::Queues;
 use crate::internal::set_name::set_name;
 use crate::semaphore::Semaphore;
@@ -99,200 +103,30 @@ impl IDevice for Device {
         let builder = vk::GraphicsPipelineCreateInfoBuilder::new();
 
         // Translate the vertex input state
-        let vertex_binding_descriptions: Vec<_> = desc
-            .vertex_layout
-            .input_bindings
-            .iter()
-            .map(|v| {
-                let input_rate = match v.input_rate {
-                    VertexInputRate::PerVertex => vk::VertexInputRate::VERTEX,
-                    VertexInputRate::PerInstance => vk::VertexInputRate::INSTANCE,
-                };
-                vk::VertexInputBindingDescriptionBuilder::new()
-                    .binding(v.binding)
-                    .stride(v.stride)
-                    .input_rate(input_rate)
-            })
-            .collect();
-        let vertex_attribute_descriptions: Vec<_> = desc
-            .vertex_layout
-            .input_attributes
-            .iter()
-            .map(|v| {
-                vk::VertexInputAttributeDescriptionBuilder::new()
-                    .location(v.location)
-                    .binding(v.binding)
-                    .offset(v.offset)
-                    .format(texture_format_to_vk(v.format))
-            })
-            .collect();
-        let vertex_input_state = vk::PipelineVertexInputStateCreateInfoBuilder::new()
-            .vertex_binding_descriptions(&vertex_binding_descriptions)
-            .vertex_attribute_descriptions(&vertex_attribute_descriptions);
+        let vertex_binding_descriptions: Vec<_> = Self::translate_vertex_bindings(desc);
+        let vertex_attribute_descriptions: Vec<_> = Self::translate_vertex_attributes(desc);
+        let vertex_input_state = Self::translate_vertex_input_state(
+            &vertex_binding_descriptions,
+            &vertex_attribute_descriptions,
+        );
+
+        let input_assembly_state = Self::translate_input_assembly_state(desc);
+        let rasterization_state = Self::translate_rasterization_state(desc);
+        let depth_stencil_state = Self::translate_depth_stencil_state(desc);
+
+        let attachments = Self::translate_color_attachment_state(desc);
+        let color_blend_state = Self::translate_color_blend_state(&attachments);
+
         let builder = builder.vertex_input_state(&vertex_input_state);
-
-        // Translate the input assembly state
-        let topology = match desc.input_assembly_state.primitive_topology {
-            PrimitiveTopology::PointList => vk::PrimitiveTopology::POINT_LIST,
-            PrimitiveTopology::LineList => vk::PrimitiveTopology::LINE_LIST,
-            PrimitiveTopology::LineStrip => vk::PrimitiveTopology::LINE_STRIP,
-            PrimitiveTopology::TriangleList => vk::PrimitiveTopology::TRIANGLE_LIST,
-            PrimitiveTopology::TriangleStrip => vk::PrimitiveTopology::TRIANGLE_STRIP,
-        };
-        let input_assembly_state = vk::PipelineInputAssemblyStateCreateInfoBuilder::new()
-            .topology(topology)
-            .primitive_restart_enable(false);
         let builder = builder.input_assembly_state(&input_assembly_state);
-
-        // Translate the rasterization state
-        let polygon_mode = match desc.rasterizer_state.polygon_mode {
-            PolygonMode::Fill => vk::PolygonMode::FILL,
-            PolygonMode::Line => vk::PolygonMode::LINE,
-        };
-        let cull_mode = match desc.rasterizer_state.cull_mode {
-            CullMode::None => vk::CullModeFlags::NONE,
-            CullMode::Back => vk::CullModeFlags::BACK,
-            CullMode::Front => vk::CullModeFlags::FRONT,
-        };
-        let front_face = match desc.rasterizer_state.front_face {
-            FrontFaceOrder::CounterClockwise => vk::FrontFace::COUNTER_CLOCKWISE,
-            FrontFaceOrder::Clockwise => vk::FrontFace::CLOCKWISE,
-        };
-        let rasterization_state = vk::PipelineRasterizationStateCreateInfoBuilder::new()
-            .polygon_mode(polygon_mode)
-            .cull_mode(cull_mode)
-            .front_face(front_face)
-            .depth_clamp_enable(todo!())
-            .rasterizer_discard_enable(false) // No support in dx12
-            .depth_bias_enable(todo!())
-            .depth_bias_constant_factor(todo!())
-            .depth_bias_clamp(0.0) // TODO: Why?
-            .depth_bias_slope_factor(todo!())
-            .line_width(1.0);
         let builder = builder.rasterization_state(&rasterization_state);
-
-        // Translate the depth/stencil state
-        const fn translate_compare_op(op: CompareOp) -> vk::CompareOp {
-            match op {
-                CompareOp::Never => vk::CompareOp::NEVER,
-                CompareOp::Always => vk::CompareOp::ALWAYS,
-                CompareOp::Equal => vk::CompareOp::EQUAL,
-                CompareOp::NotEqual => vk::CompareOp::NOT_EQUAL,
-                CompareOp::Less => vk::CompareOp::LESS,
-                CompareOp::LessEqual => vk::CompareOp::LESS_OR_EQUAL,
-                CompareOp::Greater => vk::CompareOp::GREATER,
-                CompareOp::GreaterOrEqual => vk::CompareOp::GREATER_OR_EQUAL,
-            }
-        }
-        const fn translate_stencil_op_state(
-            state: &StencilOpState,
-            compare_mask: u32,
-            write_mask: u32,
-        ) -> vk::StencilOpState {
-            const fn translate_stencil_op(op: StencilOp) -> vk::StencilOp {
-                match op {
-                    StencilOp::Keep => vk::StencilOp::KEEP,
-                    StencilOp::Zero => vk::StencilOp::ZERO,
-                    StencilOp::Replace => vk::StencilOp::REPLACE,
-                    StencilOp::IncrementClamp => vk::StencilOp::INCREMENT_AND_CLAMP,
-                    StencilOp::DecrementClamp => vk::StencilOp::DECREMENT_AND_CLAMP,
-                    StencilOp::Invert => vk::StencilOp::INVERT,
-                    StencilOp::IncrementWrap => vk::StencilOp::INCREMENT_AND_WRAP,
-                    StencilOp::DecrementWrap => vk::StencilOp::DECREMENT_AND_WRAP,
-                }
-            }
-            vk::StencilOpState {
-                fail_op: translate_stencil_op(state.fail_op),
-                pass_op: translate_stencil_op(state.pass_op),
-                depth_fail_op: translate_stencil_op(state.depth_fail_op),
-                compare_op: translate_compare_op(state.compare_op),
-                compare_mask,
-                write_mask,
-                reference: 0,
-            }
-        }
-        let depth_stencil_state = vk::PipelineDepthStencilStateCreateInfoBuilder::new()
-            .depth_test_enable(desc.depth_stencil_state.depth_test)
-            .depth_write_enable(desc.depth_stencil_state.depth_write)
-            .depth_compare_op(translate_compare_op(
-                desc.depth_stencil_state.depth_compare_op,
-            ))
-            .stencil_test_enable(desc.depth_stencil_state.stencil_test)
-            .front(translate_stencil_op_state(
-                &desc.depth_stencil_state.stencil_front,
-                desc.depth_stencil_state.stencil_read_mask as _,
-                desc.depth_stencil_state.stencil_write_mask as _,
-            ))
-            .back(translate_stencil_op_state(
-                &desc.depth_stencil_state.stencil_back,
-                desc.depth_stencil_state.stencil_read_mask as _,
-                desc.depth_stencil_state.stencil_write_mask as _,
-            ))
-            .depth_bounds_test_enable(false)
-            .min_depth_bounds(0.0)
-            .max_depth_bounds(1.0);
         let builder = builder.depth_stencil_state(&depth_stencil_state);
-
-        // Translate color blend state
-        let attachments: Vec<_> = desc
-            .blend_state
-            .attachments
-            .iter()
-            .map(|v| {
-                const fn translate_blend_factor(factor: BlendFactor) -> vk::BlendFactor {
-                    match factor {
-                        BlendFactor::Zero => vk::BlendFactor::ZERO,
-                        BlendFactor::One => vk::BlendFactor::ONE,
-                        BlendFactor::SrcColor => vk::BlendFactor::SRC_COLOR,
-                        BlendFactor::OneMinusSrcColor => vk::BlendFactor::ONE_MINUS_SRC_COLOR,
-                        BlendFactor::DstColor => vk::BlendFactor::DST_COLOR,
-                        BlendFactor::OneMinusDstColor => vk::BlendFactor::ONE_MINUS_DST_COLOR,
-                        BlendFactor::SrcAlpha => vk::BlendFactor::SRC_ALPHA,
-                        BlendFactor::OneMinusSrcAlpha => vk::BlendFactor::ONE_MINUS_SRC_ALPHA,
-                        BlendFactor::DstAlpha => vk::BlendFactor::DST_ALPHA,
-                        BlendFactor::OneMinusDstAlpha => vk::BlendFactor::ONE_MINUS_DST_ALPHA,
-                        BlendFactor::SrcAlphaSaturate => vk::BlendFactor::SRC_ALPHA_SATURATE,
-                        BlendFactor::BlendFactor => vk::BlendFactor::CONSTANT_COLOR,
-                        BlendFactor::OneMinusBlendFactor => {
-                            vk::BlendFactor::ONE_MINUS_CONSTANT_COLOR
-                        }
-                    }
-                }
-                const fn translate_blend_op(op: BlendOp) -> vk::BlendOp {
-                    match op {
-                        BlendOp::Add => vk::BlendOp::ADD,
-                        BlendOp::Subtract => vk::BlendOp::SUBTRACT,
-                        BlendOp::ReverseSubtract => vk::BlendOp::REVERSE_SUBTRACT,
-                        BlendOp::Min => vk::BlendOp::MIN,
-                        BlendOp::Max => vk::BlendOp::MAX,
-                    }
-                }
-                vk::PipelineColorBlendAttachmentStateBuilder::new()
-                    .blend_enable(v.blend_enabled)
-                    .src_color_blend_factor(translate_blend_factor(v.src_factor))
-                    .dst_color_blend_factor(translate_blend_factor(v.dst_factor))
-                    .color_blend_op(translate_blend_op(v.blend_op))
-                    .src_alpha_blend_factor(translate_blend_factor(v.alpha_src_factor))
-                    .dst_alpha_blend_factor(translate_blend_factor(v.alpha_dst_factor))
-                    .alpha_blend_op(translate_blend_op(v.alpha_blend_op))
-                    .color_write_mask(vk::ColorComponentFlags::from_bits_truncate(
-                        v.color_write_mask.bits() as _,
-                    ))
-            })
-            .collect();
-        let color_blend_state = vk::PipelineColorBlendStateCreateInfoBuilder::new()
-            .logic_op_enable(false)
-            .logic_op(vk::LogicOp::CLEAR)
-            .attachments(&attachments)
-            .blend_constants([0.0, 0.0, 0.0, 0.0]);
         let builder = builder.color_blend_state(&color_blend_state);
 
         let pipeline = unsafe {
-            self.device_loader.create_graphics_pipelines(
-                vk::PipelineCache::null(),
-                &[builder],
-                None,
-            )
+            self.device_loader
+                .create_graphics_pipelines(vk::PipelineCache::null(), &[builder], None)
+                .map_err(|v| anyhow!(v))?
         };
         let pipeline = pipeline[0];
 
@@ -525,7 +359,10 @@ impl IDevice for Device {
         match result.raw {
             vk::Result::SUCCESS => FenceWaitResult::Complete,
             vk::Result::TIMEOUT => FenceWaitResult::Timeout,
-            _ => result.unwrap(),
+            _ => {
+                result.unwrap();
+                unreachable!()
+            }
         }
     }
 
@@ -542,7 +379,10 @@ impl IDevice for Device {
         match result.raw {
             vk::Result::SUCCESS => true,
             vk::Result::NOT_READY => false,
-            _ => result.unwrap(),
+            _ => {
+                result.unwrap();
+                unreachable!()
+            }
         }
     }
 
@@ -567,6 +407,147 @@ impl IDevice for Device {
 
     fn get_backend_api(&self) -> BackendAPI {
         BackendAPI::Vulkan
+    }
+}
+
+impl Device {
+    fn translate_vertex_bindings(
+        desc: &GraphicsPipelineDesc,
+    ) -> Vec<vk::VertexInputBindingDescriptionBuilder<'static>> {
+        desc.vertex_layout
+            .input_bindings
+            .iter()
+            .map(|v| {
+                vk::VertexInputBindingDescriptionBuilder::new()
+                    .binding(v.binding)
+                    .stride(v.stride)
+                    .input_rate(vertex_input_rate_to_vk(v.input_rate))
+            })
+            .collect()
+    }
+
+    fn translate_vertex_attributes(
+        desc: &GraphicsPipelineDesc,
+    ) -> Vec<vk::VertexInputAttributeDescriptionBuilder<'static>> {
+        desc.vertex_layout
+            .input_attributes
+            .iter()
+            .map(|v| {
+                vk::VertexInputAttributeDescriptionBuilder::new()
+                    .location(v.location)
+                    .binding(v.binding)
+                    .offset(v.offset)
+                    .format(texture_format_to_vk(v.format))
+            })
+            .collect()
+    }
+
+    fn translate_vertex_input_state<'a>(
+        vertex_binding_descriptions: &'a [vk::VertexInputBindingDescriptionBuilder],
+        vertex_attribute_descriptions: &'a [vk::VertexInputAttributeDescriptionBuilder],
+    ) -> vk::PipelineVertexInputStateCreateInfoBuilder<'a> {
+        vk::PipelineVertexInputStateCreateInfoBuilder::new()
+            .vertex_binding_descriptions(vertex_binding_descriptions)
+            .vertex_attribute_descriptions(vertex_attribute_descriptions)
+    }
+
+    fn translate_input_assembly_state(
+        desc: &GraphicsPipelineDesc,
+    ) -> vk::PipelineInputAssemblyStateCreateInfoBuilder<'static> {
+        let topology = primitive_topology_to_vk(desc.input_assembly_state.primitive_topology);
+        vk::PipelineInputAssemblyStateCreateInfoBuilder::new()
+            .topology(topology)
+            .primitive_restart_enable(false)
+    }
+
+    fn translate_rasterization_state(
+        desc: &GraphicsPipelineDesc,
+    ) -> vk::PipelineRasterizationStateCreateInfoBuilder<'static> {
+        let polygon_mode = polygon_mode_to_vk(desc.rasterizer_state.polygon_mode);
+        let cull_mode = cull_mode_to_vk(desc.rasterizer_state.cull_mode);
+        let front_face = front_face_order_to_vk(desc.rasterizer_state.front_face);
+        vk::PipelineRasterizationStateCreateInfoBuilder::new()
+            .polygon_mode(polygon_mode)
+            .cull_mode(cull_mode)
+            .front_face(front_face)
+            .depth_clamp_enable(true)
+            .rasterizer_discard_enable(false) // No support in dx12
+            .depth_bias_enable(false)
+            .depth_bias_constant_factor(0.0)
+            .depth_bias_clamp(0.0)
+            .depth_bias_slope_factor(0.0)
+            .line_width(1.0)
+    }
+
+    fn translate_depth_stencil_state(
+        desc: &GraphicsPipelineDesc,
+    ) -> vk::PipelineDepthStencilStateCreateInfoBuilder<'static> {
+        const fn translate_stencil_op_state(
+            state: &StencilOpState,
+            compare_mask: u32,
+            write_mask: u32,
+        ) -> vk::StencilOpState {
+            vk::StencilOpState {
+                fail_op: stencil_op_to_vk(state.fail_op),
+                pass_op: stencil_op_to_vk(state.pass_op),
+                depth_fail_op: stencil_op_to_vk(state.depth_fail_op),
+                compare_op: compare_op_to_vk(state.compare_op),
+                compare_mask,
+                write_mask,
+                reference: 0,
+            }
+        }
+
+        vk::PipelineDepthStencilStateCreateInfoBuilder::new()
+            .depth_test_enable(desc.depth_stencil_state.depth_test)
+            .depth_write_enable(desc.depth_stencil_state.depth_write)
+            .depth_compare_op(compare_op_to_vk(desc.depth_stencil_state.depth_compare_op))
+            .stencil_test_enable(desc.depth_stencil_state.stencil_test)
+            .front(translate_stencil_op_state(
+                &desc.depth_stencil_state.stencil_front,
+                desc.depth_stencil_state.stencil_read_mask as _,
+                desc.depth_stencil_state.stencil_write_mask as _,
+            ))
+            .back(translate_stencil_op_state(
+                &desc.depth_stencil_state.stencil_back,
+                desc.depth_stencil_state.stencil_read_mask as _,
+                desc.depth_stencil_state.stencil_write_mask as _,
+            ))
+            .depth_bounds_test_enable(desc.depth_stencil_state.depth_bounds_enable)
+            .min_depth_bounds(desc.depth_stencil_state.min_depth_bounds)
+            .max_depth_bounds(desc.depth_stencil_state.max_depth_bounds)
+    }
+
+    fn translate_color_attachment_state(
+        desc: &GraphicsPipelineDesc,
+    ) -> Vec<vk::PipelineColorBlendAttachmentStateBuilder<'static>> {
+        desc.blend_state
+            .attachments
+            .iter()
+            .map(|v| {
+                vk::PipelineColorBlendAttachmentStateBuilder::new()
+                    .blend_enable(v.blend_enabled)
+                    .src_color_blend_factor(blend_factor_to_vk(v.src_factor))
+                    .dst_color_blend_factor(blend_factor_to_vk(v.dst_factor))
+                    .color_blend_op(blend_op_to_vk(v.blend_op))
+                    .src_alpha_blend_factor(blend_factor_to_vk(v.alpha_src_factor))
+                    .dst_alpha_blend_factor(blend_factor_to_vk(v.alpha_dst_factor))
+                    .alpha_blend_op(blend_op_to_vk(v.alpha_blend_op))
+                    .color_write_mask(vk::ColorComponentFlags::from_bits_truncate(
+                        v.color_write_mask.bits() as _,
+                    ))
+            })
+            .collect()
+    }
+
+    fn translate_color_blend_state<'a>(
+        attachments: &'a [vk::PipelineColorBlendAttachmentStateBuilder],
+    ) -> vk::PipelineColorBlendStateCreateInfoBuilder<'a> {
+        vk::PipelineColorBlendStateCreateInfoBuilder::new()
+            .logic_op_enable(false)
+            .logic_op(vk::LogicOp::CLEAR)
+            .attachments(attachments)
+            .blend_constants([0.0, 0.0, 0.0, 0.0])
     }
 }
 
--- lib/plugins/aleph-gpu-vulkan/src/internal/conv.rs
+++ lib/plugins/aleph-gpu-vulkan/src/internal/conv.rs
@@ -87,3 +87,96 @@ pub const fn present_mode_to_vk(mode: PresentationMode) -> vk::PresentModeKHR {
         PresentationMode::Fifo => vk::PresentModeKHR::FIFO_KHR,
     }
 }
+
+pub const fn stencil_op_to_vk(op: StencilOp) -> vk::StencilOp {
+    match op {
+        StencilOp::Keep => vk::StencilOp::KEEP,
+        StencilOp::Zero => vk::StencilOp::ZERO,
+        StencilOp::Replace => vk::StencilOp::REPLACE,
+        StencilOp::IncrementClamp => vk::StencilOp::INCREMENT_AND_CLAMP,
+        StencilOp::DecrementClamp => vk::StencilOp::DECREMENT_AND_CLAMP,
+        StencilOp::Invert => vk::StencilOp::INVERT,
+        StencilOp::IncrementWrap => vk::StencilOp::INCREMENT_AND_WRAP,
+        StencilOp::DecrementWrap => vk::StencilOp::DECREMENT_AND_WRAP,
+    }
+}
+
+pub const fn compare_op_to_vk(op: CompareOp) -> vk::CompareOp {
+    match op {
+        CompareOp::Never => vk::CompareOp::NEVER,
+        CompareOp::Always => vk::CompareOp::ALWAYS,
+        CompareOp::Equal => vk::CompareOp::EQUAL,
+        CompareOp::NotEqual => vk::CompareOp::NOT_EQUAL,
+        CompareOp::Less => vk::CompareOp::LESS,
+        CompareOp::LessEqual => vk::CompareOp::LESS_OR_EQUAL,
+        CompareOp::Greater => vk::CompareOp::GREATER,
+        CompareOp::GreaterOrEqual => vk::CompareOp::GREATER_OR_EQUAL,
+    }
+}
+
+pub const fn primitive_topology_to_vk(topology: PrimitiveTopology) -> vk::PrimitiveTopology {
+    match topology {
+        PrimitiveTopology::PointList => vk::PrimitiveTopology::POINT_LIST,
+        PrimitiveTopology::LineList => vk::PrimitiveTopology::LINE_LIST,
+        PrimitiveTopology::LineStrip => vk::PrimitiveTopology::LINE_STRIP,
+        PrimitiveTopology::TriangleList => vk::PrimitiveTopology::TRIANGLE_LIST,
+        PrimitiveTopology::TriangleStrip => vk::PrimitiveTopology::TRIANGLE_STRIP,
+    }
+}
+
+pub const fn polygon_mode_to_vk(mode: PolygonMode) -> vk::PolygonMode {
+    match mode {
+        PolygonMode::Fill => vk::PolygonMode::FILL,
+        PolygonMode::Line => vk::PolygonMode::LINE,
+    }
+}
+
+pub const fn front_face_order_to_vk(order: FrontFaceOrder) -> vk::FrontFace {
+    match order {
+        FrontFaceOrder::CounterClockwise => vk::FrontFace::COUNTER_CLOCKWISE,
+        FrontFaceOrder::Clockwise => vk::FrontFace::CLOCKWISE,
+    }
+}
+
+pub const fn cull_mode_to_vk(cull_mode: CullMode) -> vk::CullModeFlags {
+    match cull_mode {
+        CullMode::None => vk::CullModeFlags::NONE,
+        CullMode::Back => vk::CullModeFlags::BACK,
+        CullMode::Front => vk::CullModeFlags::FRONT,
+    }
+}
+
+pub const fn vertex_input_rate_to_vk(input_rate: VertexInputRate) -> vk::VertexInputRate {
+    match input_rate {
+        VertexInputRate::PerVertex => vk::VertexInputRate::VERTEX,
+        VertexInputRate::PerInstance => vk::VertexInputRate::INSTANCE,
+    }
+}
+
+pub const fn blend_factor_to_vk(factor: BlendFactor) -> vk::BlendFactor {
+    match factor {
+        BlendFactor::Zero => vk::BlendFactor::ZERO,
+        BlendFactor::One => vk::BlendFactor::ONE,
+        BlendFactor::SrcColor => vk::BlendFactor::SRC_COLOR,
+        BlendFactor::OneMinusSrcColor => vk::BlendFactor::ONE_MINUS_SRC_COLOR,
+        BlendFactor::DstColor => vk::BlendFactor::DST_COLOR,
+        BlendFactor::OneMinusDstColor => vk::BlendFactor::ONE_MINUS_DST_COLOR,
+        BlendFactor::SrcAlpha => vk::BlendFactor::SRC_ALPHA,
+        BlendFactor::OneMinusSrcAlpha => vk::BlendFactor::ONE_MINUS_SRC_ALPHA,
+        BlendFactor::DstAlpha => vk::BlendFactor::DST_ALPHA,
+        BlendFactor::OneMinusDstAlpha => vk::BlendFactor::ONE_MINUS_DST_ALPHA,
+        BlendFactor::SrcAlphaSaturate => vk::BlendFactor::SRC_ALPHA_SATURATE,
+        BlendFactor::BlendFactor => vk::BlendFactor::CONSTANT_COLOR,
+        BlendFactor::OneMinusBlendFactor => vk::BlendFactor::ONE_MINUS_CONSTANT_COLOR,
+    }
+}
+
+pub const fn blend_op_to_vk(op: BlendOp) -> vk::BlendOp {
+    match op {
+        BlendOp::Add => vk::BlendOp::ADD,
+        BlendOp::Subtract => vk::BlendOp::SUBTRACT,
+        BlendOp::ReverseSubtract => vk::BlendOp::REVERSE_SUBTRACT,
+        BlendOp::Min => vk::BlendOp::MIN,
+        BlendOp::Max => vk::BlendOp::MAX,
+    }
+}
