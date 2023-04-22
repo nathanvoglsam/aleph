0bb33aa39b78451bbe7155d913aa71e83a094fae
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2023-04-22 19:21:14 +1000

    [aleph-vulkan-alloc] Remove cmake for building

--- lib/crates/aleph-vulkan-alloc/build.rs
+++ lib/crates/aleph-vulkan-alloc/build.rs
@@ -30,69 +30,47 @@
 extern crate aleph_compile as compile;
 extern crate aleph_target_build as target;
 
-use target::build::target_architecture;
-use target::build::{target_build_config, target_platform};
-use target::Architecture;
+use std::path::Path;
 
 fn main() {
-    let mut build = cmake::Config::new("library");
+    let vk_header_inc = Path::new("../../../submodules/Vulkan-Headers/include");
+    let vma_header_inc = Path::new("../../../submodules/VulkanMemoryAllocator/src");
+
+    let mut build = cc::Build::new();
+    build.cpp(true);
+    build.define("VMA_STATIC_VULKAN_FUNCTIONS", "0");
+    build.file("library/vk_mem_alloc.cpp");
+    build.include(vk_header_inc);
+    build.include(vma_header_inc);
 
     if cfg!(feature = "corruption_detection") {
-        build.define("FEATURE_VMA_DEBUG_DETECT_CORRUPTION", "1");
+        build.define("VMA_DEBUG_DETECT_CORRUPTION", "1");
     }
 
-    let optimized = target_build_config().is_optimized();
-    let debug = target_build_config().is_debug();
-    match (optimized, debug) {
-        (true, true) => {
-            build.profile("RelWithDebInfo");
-        }
-        (false, true) => {
-            build.profile("Debug");
-        }
-        (_, false) => {
-            build.profile("Release");
-        }
-    }
+    // if target_platform().is_android() {
+    //     let android_home = std::env::var("ANDROID_HOME").unwrap();
+    //     let toolchain_file = format!(
+    //         "{}/ndk-bundle/build/cmake/android.toolchain.cmake",
+    //         &android_home
+    //     );
+    //
+    //     build.define("CMAKE_TOOLCHAIN_FILE", &toolchain_file);
+    //     build.define("ANDROID_PLATFORM", "android-24");
+    //     build.define("ANDROID_STL", "c++_shared");
+    //     build.define("ANDROID_ABI", target_architecture().ndk_name());
+    //
+    //     match target_architecture() {
+    //         Architecture::X8664 => {
+    //             build.target("x86_64-none-linux-android24");
+    //         }
+    //         Architecture::AARCH64 => {
+    //             build.target("aarch64-none-linux-android24");
+    //         }
+    //         Architecture::Unknown => {
+    //             panic!("Unsupported architecture");
+    //         }
+    //     }
+    // }
 
-    // Force link stdc++
-    if target_platform().is_gnu() && target_platform().is_windows() {
-        println!("cargo:rustc-link-lib=dylib=stdc++");
-    }
-
-    if target_platform().is_android() {
-        let android_home = std::env::var("ANDROID_HOME").unwrap();
-        let toolchain_file = format!(
-            "{}/ndk-bundle/build/cmake/android.toolchain.cmake",
-            &android_home
-        );
-
-        build.define("CMAKE_TOOLCHAIN_FILE", &toolchain_file);
-        build.define("ANDROID_PLATFORM", "android-24");
-        build.define("ANDROID_STL", "c++_shared");
-        build.define("ANDROID_ABI", target_architecture().ndk_name());
-
-        match target_architecture() {
-            Architecture::X8664 => {
-                build.target("x86_64-none-linux-android24");
-            }
-            Architecture::AARCH64 => {
-                build.target("aarch64-none-linux-android24");
-            }
-            Architecture::Unknown => {
-                panic!("Unsupported architecture");
-            }
-        }
-    }
-
-    if target_platform().is_msvc() {
-        build.generator("Visual Studio 17 2022");
-    } else {
-        build.generator("Ninja");
-    }
-
-    let output_dir = build.build();
-
-    println!("cargo:rustc-link-search=native={}", output_dir.display());
-    //println!("cargo:rustc-link-lib=static=vma");
+    build.compile("vma");
 }
