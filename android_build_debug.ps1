28850ea89b9379e65405ca34712e6beddd82d3e9
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2023-06-17 21:06:12 +1000

    [misc] Add android build helper scripts

---
+++ android_build_debug.ps1
@@ -0,0 +1,5 @@
+$Env:ANDROID_NDK_HOME = "C:\Users\Nathan\Programs\Loose\android-ndk-r25c";
+$Env:CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER = "$Env:ANDROID_NDK_HOME\toolchains\llvm\prebuilt\windows-x86_64\bin\aarch64-linux-android30-clang.cmd";
+$Env:CARGO_TARGET_AARCH64_LINUX_ANDROID_AR = "$Env:ANDROID_NDK_HOME\toolchains\llvm\prebuilt\windows-x86_64\bin\aarch64-linux-android-ar";
+
+cargo build --target=aarch64-linux-android
---
+++ android_build_retail.ps1
@@ -0,0 +1,6 @@
+$Env:ANDROID_NDK_HOME = "C:\Users\Nathan\Programs\Loose\android-ndk-r25c";
+$Env:CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER = "$Env:ANDROID_NDK_HOME\toolchains\llvm\prebuilt\windows-x86_64\bin\aarch64-linux-android30-clang.cmd";
+$Env:CARGO_TARGET_AARCH64_LINUX_ANDROID_AR = "$Env:ANDROID_NDK_HOME\toolchains\llvm\prebuilt\windows-x86_64\bin\aarch64-linux-android-ar";
+
+$Env:ALEPH_BUILD_TYPE="Retail";
+cargo build --profile=retail --target=aarch64-linux-android
