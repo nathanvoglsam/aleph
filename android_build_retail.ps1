7ea770d9bfdbfeefa1b2bdf77b91a99f569bd88f
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2023-06-19 19:22:24 +1000

    [misc] Update android build scripts to use cargo ndk

--- android_build_retail.ps1
+++ android_build_retail.ps1
@@ -1,15 +1,11 @@
 function build {
     $OLD_ANDROID_NDK_HOME = $Env:ANDROID_NDK_HOME;
     $Env:ANDROID_NDK_HOME = "C:\Users\Nathan\Programs\Loose\android-ndk-r25c";
-    $Env:CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER = "$Env:ANDROID_NDK_HOME\toolchains\llvm\prebuilt\windows-x86_64\bin\clang";
-    $Env:RUSTFLAGS = "-C link-args=--target=aarch64-linux-android30";
 
     $Env:ALEPH_BUILD_TYPE="Retail";
-    cargo build --profile=retail --target=aarch64-linux-android --package aleph-test --lib
+    cargo ndk -t arm64-v8a -p 30 build --profile=retail --target=aarch64-linux-android --package aleph-test --lib
 
     $Env:ANDROID_NDK_HOME = $OLD_ANDROID_NDK_HOME;
-    $Env:CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER = "";
-    $Env:RUSTFLAGS = "";
     $Env:ALEPH_BUILD_TYPE="";
 }
 
