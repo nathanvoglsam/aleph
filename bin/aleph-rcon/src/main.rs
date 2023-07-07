1d56f772e480ce625920da3df8a25fe5d6e76ce4
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2023-07-07 18:24:59 +1000

    [aleph-rcon] Use windows_system = "windows" always

--- bin/aleph-rcon/src/main.rs
+++ bin/aleph-rcon/src/main.rs
@@ -27,7 +27,7 @@
 // SOFTWARE.
 //
 
-#![cfg_attr(target_vendor = "uwp", windows_subsystem = "windows")]
+#![windows_subsystem = "windows"]
 
 #[cfg(target_os = "windows")]
 aleph_dx12_agility_sdk::export_standard_agility_sdk_symbols!();
