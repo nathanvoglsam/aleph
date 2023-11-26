8f5c938d05384799700f1133987a177c96c98b43
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2023-11-26 16:16:11 +1100

    [aleph-ecs] Update 'Schedule' to replace internal 'Box' usage with allocations from an internal bump arena

--- lib/crates/aleph-ecs/src/scheduler/mod.rs
+++ lib/crates/aleph-ecs/src/scheduler/mod.rs
@@ -63,8 +63,12 @@ pub use system_schedule::SystemSchedule;
 
 use crate::system::{IntoSystem, System};
 use crate::world::World;
+use aleph_arena_drop_list::DropLink;
 use aleph_label::Label;
+use bumpalo::Bump;
 use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+use std::ptr::NonNull;
 
 ///
 /// Provides an interface for dynamically composing a set of executable code blocks into a sequence
@@ -77,11 +81,18 @@ use std::collections::HashMap;
 ///
 #[derive(Default)]
 pub struct Schedule {
+    /// A bump allocated arena used for storing the labels and stages.
+    arena: Bump,
+
     /// Stores the execution stages along with the label they were registered with
-    stages: HashMap<Box<dyn Label>, Box<dyn Stage>>,
+    stages: HashMap<LabelKey, NonNull<dyn Stage>>,
+
+    /// The head of a linked list of all objects stored in the schedule's bump arena that need to
+    /// be dropped when the Schedule is destroyed. This includes all the labels, stages and systems.
+    drop_head: Option<NonNull<DropLink>>,
 
     /// Stores the execution order of the schedule as an ordered sequence of stage labels
-    stage_order: Vec<Box<dyn Label>>,
+    stage_order: Vec<NonNull<dyn Label>>,
 
     /// Stores the schedule's run criteria system which checks prior to executing the stages whether
     /// the schedule should actually run.
@@ -108,7 +119,10 @@ impl Schedule {
         &mut self,
         system: S,
     ) -> &mut Self {
-        self.run_criteria.system = Some(Box::new(system.system()));
+        let system = self.arena.alloc(system.system());
+        let system = NonNull::from(system);
+        DropLink::append_drop_list(&self.arena, &mut self.drop_head, system);
+        self.run_criteria.system = Some(NonNull::from(system));
         self.run_criteria.initialized = false;
         self
     }
@@ -117,10 +131,17 @@ impl Schedule {
     /// stage will be appended to the very end of the execution order, so it will run last.
     #[inline]
     pub fn add_stage<S: Stage>(&mut self, label: impl Label, stage: S) -> &mut Self {
-        let label: Box<dyn Label> = Box::new(label);
-        self.stage_order.push(label.clone());
-        let prev = self.stages.insert(label.clone(), Box::new(stage));
+        let label = self.arena.alloc(label);
+        let label = NonNull::from(label);
+        DropLink::append_drop_list(&self.arena, &mut self.drop_head, label);
+        self.stage_order.push(label);
+
+        let stage = self.arena.alloc(stage);
+        let stage = NonNull::from(stage);
+        DropLink::append_drop_list(&self.arena, &mut self.drop_head, stage);
+        let prev = self.stages.insert(LabelKey(label), stage);
         if prev.is_some() {
+            let label = unsafe { label.as_ref() };
             panic!("Stage already exists: {label:?}.");
         }
         self
@@ -136,7 +157,9 @@ impl Schedule {
         stage: S,
     ) -> &mut Self {
         // Box the label for the stage we're adding
-        let label: Box<dyn Label> = Box::new(label);
+        let label = self.arena.alloc(label);
+        let label = NonNull::from(label);
+        DropLink::append_drop_list(&self.arena, &mut self.drop_head, label);
 
         // Lookup the index of the stage that the new stage should execute after
         let target = &target as &dyn Label;
@@ -145,13 +168,17 @@ impl Schedule {
         // Insert the stage's label into the execution order directly after the prerequisite stage.
         //
         // This fulfills the requirement for ensuring the new stage runs after the requested stage.
-        self.stage_order.insert(target_index + 1, label.clone());
+        self.stage_order.insert(target_index + 1, label);
 
         // Insert the new stage into the stage storage. We panic if there was already a stage
         // registered with the provided label as we do not allow overwriting already provided
         // stages.
-        let prev = self.stages.insert(label.clone(), Box::new(stage));
+        let stage = self.arena.alloc(stage);
+        let stage = NonNull::from(stage);
+        DropLink::append_drop_list(&self.arena, &mut self.drop_head, stage);
+        let prev = self.stages.insert(LabelKey(label), stage);
         if prev.is_some() {
+            let label = unsafe { label.as_ref() };
             panic!("Stage already exists: {label:?}.");
         }
         self
@@ -167,7 +194,9 @@ impl Schedule {
         stage: S,
     ) -> &mut Self {
         // Box the label for the stage we're adding
-        let label: Box<dyn Label> = Box::new(label);
+        let label = self.arena.alloc(label);
+        let label = NonNull::from(label);
+        DropLink::append_drop_list(&self.arena, &mut self.drop_head, label);
 
         // Lookup the index of the stage that the new stage should execute before
         let target = &target as &dyn Label;
@@ -177,13 +206,17 @@ impl Schedule {
         // stage needs to run before.
         //
         // This fulfills the requirement for ensuring the new stage runs before the requested stage.
-        self.stage_order.insert(target_index, label.clone());
+        self.stage_order.insert(target_index, label);
 
         // Insert the new stage into the stage storage. We panic if there was already a stage
         // registered with the provided label as we do not allow overwriting already provided
         // stages.
-        let prev = self.stages.insert(label.clone(), Box::new(stage));
+        let stage = self.arena.alloc(stage);
+        let stage = NonNull::from(stage);
+        DropLink::append_drop_list(&self.arena, &mut self.drop_head, stage);
+        let prev = self.stages.insert(LabelKey(label), stage);
         if prev.is_some() {
+            let label = unsafe { label.as_ref() };
             panic!("Stage already exists: {label:?}.");
         }
         self
@@ -278,16 +311,16 @@ impl Schedule {
     #[inline]
     pub fn get_stage<T: Stage>(&self, label: &dyn Label) -> Option<&T> {
         self.stages
-            .get(label)
-            .and_then(|stage| stage.downcast_ref::<T>())
+            .get(&LabelKey(NonNull::from(label)))
+            .and_then(|stage| unsafe { stage.as_ref().downcast_ref::<T>() })
     }
 
     /// Get's a down-casted reference to the stage registered with the [`Label`] provided in `label`
     #[inline]
     pub fn get_stage_mut<T: Stage>(&mut self, label: &dyn Label) -> Option<&mut T> {
         self.stages
-            .get_mut(label)
-            .and_then(|stage| stage.downcast_mut::<T>())
+            .get_mut(&LabelKey(NonNull::from(label)))
+            .and_then(|stage| unsafe { stage.as_mut().downcast_mut::<T>() })
     }
 
     /// Unconditionally (i.e, the run criteria system is **not** called) performs a single execution
@@ -295,8 +328,10 @@ impl Schedule {
     #[inline]
     pub fn run_once(&mut self, world: &mut World) {
         for label in self.stage_order.iter() {
-            let stage = self.stages.get_mut(label).unwrap();
-            stage.run(world);
+            let stage = self.stages.get_mut(&LabelKey(*label)).unwrap();
+            unsafe {
+                stage.as_mut().run(world);
+            }
         }
     }
 
@@ -305,17 +340,17 @@ impl Schedule {
     pub fn iter_stages(&self) -> impl Iterator<Item = (&dyn Label, &dyn Stage)> {
         self.stage_order
             .iter()
-            .map(move |label| (&**label, &*self.stages[label]))
+            .map(move |label| unsafe { (label.as_ref(), self.stages[&LabelKey(*label)].as_ref()) })
     }
 }
 
 /// Internal utility functions
 impl Schedule {
-    fn index_from_label(&mut self, target: &dyn Label) -> usize {
+    fn index_from_label(&self, target: &dyn Label) -> usize {
         self.stage_order
             .iter()
             .enumerate()
-            .find(|(_i, stage_label)| &***stage_label == target)
+            .find(|(_i, stage_label)| unsafe { stage_label.as_ref() == target })
             .map(|(i, _)| i)
             .unwrap_or_else(|| panic!("Target stage does not exist: {target:?}."))
     }
@@ -332,6 +367,7 @@ impl Stage for Schedule {
             }
 
             // Execute the system and bail if it decides we should not run the schedule
+            let system = unsafe { system.as_mut() };
             if system.execute_safe((), world) == ShouldRun::No {
                 return;
             }
@@ -339,6 +375,16 @@ impl Stage for Schedule {
 
         // If we pass the above check then we can continue on and execute the schedule
         self.run_once(world);
+    }
+}
+
+impl Drop for Schedule {
+    fn drop(&mut self) {
+        // Safety: Drops all the objects that were allocated from the bump arena by walking the
+        //         linked list we build while adding them to the schedule.
+        unsafe {
+            DropLink::drop_and_null(&mut self.drop_head);
+        }
     }
 }
 
@@ -353,6 +399,25 @@ pub enum ShouldRun {
 
 #[derive(Default)]
 struct RunCriteriaBox {
-    system: Option<Box<dyn System<In = (), Out = ShouldRun>>>,
+    system: Option<NonNull<dyn System<In = (), Out = ShouldRun>>>,
     initialized: bool,
+}
+
+#[repr(transparent)]
+struct LabelKey(NonNull<dyn Label>);
+
+impl PartialEq for LabelKey {
+    fn eq(&self, other: &Self) -> bool {
+        unsafe { self.0.as_ref().eq(other.0.as_ref()) }
+    }
+}
+
+impl Eq for LabelKey {}
+
+impl Hash for LabelKey {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        unsafe {
+            self.0.as_ref().dyn_hash(state);
+        }
+    }
 }
