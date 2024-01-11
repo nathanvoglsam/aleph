9d89484c52d12d6ca74d09eb8b560c23e5a83e79
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2024-01-11 19:12:19 +1100

    [aleph-egui] Use new DPI API

--- lib/plugins/aleph-egui/src/utils.rs
+++ lib/plugins/aleph-egui/src/utils.rs
@@ -38,13 +38,12 @@ pub fn get_egui_input(
     frame_timer: &dyn IFrameTimer,
     events: &dyn IEvents,
 ) -> egui::RawInput {
-    let drawable_size = window.drawable_size();
     let window_size = window.size();
 
     let screen_rect = egui::Pos2::new(window_size.0 as f32, window_size.1 as f32);
     let screen_rect = Some(egui::Rect::from_min_max(Default::default(), screen_rect));
 
-    let pixels_per_point = Some(drawable_size.0 as f32 / window_size.0 as f32);
+    let pixels_per_point = Some(window.current_display_scale());
 
     let time = Some(frame_timer.elapsed_time());
 
