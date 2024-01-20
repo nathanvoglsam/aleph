490303cb2e408f99b8924d95e2a03492eb1b9004
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2024-01-20 20:15:34 +1100

    [aleph-shader-db] Add super quick shader database format

---
+++ lib/crates/aleph-shader-db/src/lib.rs
@@ -0,0 +1,78 @@
+//
+//
+// This file is a part of Aleph
+//
+// https://github.com/nathanvoglsam/aleph
+//
+// MIT License
+//
+// Copyright (c) 2020 Aleph Engine
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+use std::collections::HashMap;
+
+use rkyv::{Archive, Deserialize, Serialize};
+
+#[derive(Default, Archive, Serialize, Deserialize)]
+pub struct ShaderDatabase {
+    pub shaders: HashMap<String, ShaderEntry>,
+}
+
+#[derive(Default, Archive, Serialize, Deserialize)]
+pub struct ShaderEntry {
+    pub spirv: Vec<u8>,
+    pub dxil: Vec<u8>,
+}
+
+pub struct ShaderEntryRef<'a> {
+    pub spirv: &'a [u8],
+    pub dxil: &'a [u8],
+}
+
+pub trait IShaderDatabase {
+    fn get(&self, name: &str) -> Option<ShaderEntryRef>;
+}
+
+impl IShaderDatabase for ShaderDatabase {
+    fn get(&self, name: &str) -> Option<ShaderEntryRef> {
+        if let Some(v) = self.shaders.get(name) {
+            Some(ShaderEntryRef {
+                spirv: &v.spirv,
+                dxil: &v.dxil,
+            })
+        } else {
+            None
+        }
+    }
+}
+
+impl IShaderDatabase for ArchivedShaderDatabase {
+    fn get(&self, name: &str) -> Option<ShaderEntryRef> {
+        if let Some(v) = self.shaders.get(name) {
+            Some(ShaderEntryRef {
+                spirv: &v.spirv,
+                dxil: &v.dxil,
+            })
+        } else {
+            None
+        }
+    }
+}
