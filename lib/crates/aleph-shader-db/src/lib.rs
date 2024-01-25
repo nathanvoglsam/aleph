86597a65a18f3ff7926434d38c34cfca3827e70b
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2024-01-25 22:31:46 +1100

    [aleph-shader-db] A bunch of utilities for more type safer shader loading

--- lib/crates/aleph-shader-db/src/lib.rs
+++ lib/crates/aleph-shader-db/src/lib.rs
@@ -27,34 +27,116 @@
 // SOFTWARE.
 //
 
-use std::collections::HashMap;
-
 use rkyv::{Archive, Deserialize, Serialize};
+use std::collections::HashMap;
+use std::marker::PhantomData;
+
+#[derive(
+    Archive, Serialize, Deserialize, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug,
+)]
+pub enum ShaderType {
+    Vertex,
+    Fragment,
+    Compute,
+    Geometry,
+}
+
+pub trait ShaderStage {
+    fn shader_type_matches(v: ShaderType) -> bool;
+}
+
+pub struct Vertex {}
+
+impl ShaderStage for Vertex {
+    fn shader_type_matches(v: ShaderType) -> bool {
+        v == ShaderType::Vertex
+    }
+}
+
+pub struct Fragment {}
+
+impl ShaderStage for Fragment {
+    fn shader_type_matches(v: ShaderType) -> bool {
+        v == ShaderType::Fragment
+    }
+}
+
+pub struct Compute {}
+
+impl ShaderStage for Compute {
+    fn shader_type_matches(v: ShaderType) -> bool {
+        v == ShaderType::Compute
+    }
+}
+
+pub struct Geometry {}
+
+impl ShaderStage for Geometry {
+    fn shader_type_matches(v: ShaderType) -> bool {
+        v == ShaderType::Geometry
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct ShaderName<'a, T: ShaderStage> {
+    v: &'a str,
+    _phantom: PhantomData<T>,
+}
+
+impl<'a, T: ShaderStage> ShaderName<'a, T> {
+    pub unsafe fn new(v: &'a str) -> Self {
+        Self {
+            v,
+            _phantom: PhantomData,
+        }
+    }
+}
 
 #[derive(Default, Archive, Serialize, Deserialize)]
 pub struct ShaderDatabase {
     pub shaders: HashMap<String, ShaderEntry>,
 }
 
-#[derive(Default, Archive, Serialize, Deserialize)]
+#[derive(Archive, Serialize, Deserialize)]
 pub struct ShaderEntry {
+    pub shader_type: ShaderType,
     pub spirv: Vec<u8>,
     pub dxil: Vec<u8>,
 }
 
 pub struct ShaderEntryRef<'a> {
+    pub shader_type: ShaderType,
     pub spirv: &'a [u8],
     pub dxil: &'a [u8],
 }
 
 pub trait IShaderDatabase {
-    fn get(&self, name: &str) -> Option<ShaderEntryRef>;
+    fn get_by_name(&self, name: &str) -> Option<ShaderEntryRef>;
+}
+
+pub trait IShaderDatabaseExt {
+    fn get<T: ShaderStage>(&self, name: ShaderName<T>) -> Option<ShaderEntryRef>;
+}
+
+impl<T: IShaderDatabase + ?Sized> IShaderDatabaseExt for T {
+    fn get<S: ShaderStage>(&self, name: ShaderName<S>) -> Option<ShaderEntryRef> {
+        self.get_by_name(name.v)
+            .map(|v| {
+                if S::shader_type_matches(v.shader_type) {
+                    Some(v)
+                } else {
+                    None
+                }
+            })
+            .flatten()
+    }
 }
 
 impl IShaderDatabase for ShaderDatabase {
-    fn get(&self, name: &str) -> Option<ShaderEntryRef> {
+    fn get_by_name(&self, name: &str) -> Option<ShaderEntryRef> {
         if let Some(v) = self.shaders.get(name) {
             Some(ShaderEntryRef {
+                shader_type: v.shader_type,
                 spirv: &v.spirv,
                 dxil: &v.dxil,
             })
@@ -65,9 +147,10 @@ impl IShaderDatabase for ShaderDatabase {
 }
 
 impl IShaderDatabase for ArchivedShaderDatabase {
-    fn get(&self, name: &str) -> Option<ShaderEntryRef> {
+    fn get_by_name(&self, name: &str) -> Option<ShaderEntryRef> {
         if let Some(v) = self.shaders.get(name) {
             Some(ShaderEntryRef {
+                shader_type: v.shader_type.deserialize(&mut rkyv::Infallible).unwrap(),
                 spirv: &v.spirv,
                 dxil: &v.dxil,
             })
