3fc5f8bb7d159d210dacb7b13c1260b415f2a9a0
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2024-02-18 15:29:51 +1100

    [aleph-rhi-null] Update for API changes

--- lib/crates/aleph-rhi-null/src/descriptor_pool.rs
+++ lib/crates/aleph-rhi-null/src/descriptor_pool.rs
@@ -49,17 +49,6 @@ impl IDescriptorPool for NullDescriptorPool {
         Ok(unsafe { DescriptorSetHandle::from_raw_int(handle).unwrap() })
     }
 
-    fn allocate_sets(
-        &mut self,
-        num_sets: usize,
-    ) -> Result<Vec<DescriptorSetHandle>, DescriptorPoolAllocateError> {
-        let mut sets = Vec::with_capacity(num_sets);
-        for _ in 0..num_sets {
-            sets.push(self.allocate_set()?);
-        }
-        Ok(sets)
-    }
-
     unsafe fn free(&mut self, _sets: &[DescriptorSetHandle]) {
         self.counter = 1;
     }
