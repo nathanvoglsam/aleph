666870f62c2db631ce326e959ae0a0520de069f4
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2024-02-18 23:18:30 +1100

    [aleph-rhi-validation] API update

--- lib/crates/aleph-rhi-validation/src/queue.rs
+++ lib/crates/aleph-rhi-validation/src/queue.rs
@@ -180,7 +180,7 @@ impl IQueue for ValidationQueue {
         })
     }
 
-    unsafe fn present(&self, desc: &QueuePresentDesc) -> Result<bool, QueuePresentError> {
+    unsafe fn present(&self, desc: &QueuePresentDesc) -> Result<(), QueuePresentError> {
         let swap_chain = desc
             .swap_chain
             .query_interface::<ValidationSwapChain>()
