ae97c4dfe417ea866993bd4132ca886d4c39a3ad
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2024-06-05 21:39:04 +1000

    [aleph-render] Add more upload manager utility code

--- lib/plugins/aleph-render/src/upload_manager/mod.rs
+++ lib/plugins/aleph-render/src/upload_manager/mod.rs
@@ -55,6 +55,15 @@ pub struct TextureUploadDesc {
 }
 
 impl TextureUploadDesc {
+    pub const fn new(width: u32, height: u32, depth: u32, format: Format) -> Self {
+        Self {
+            width,
+            height,
+            depth,
+            format,
+        }
+    }
+
     /// Computes the number of bytes needed to store the texture slice described by this
     /// [TextureUploadDesc] in a format compatible with being uploaded using a buffer to texture
     /// copy.
@@ -64,15 +73,28 @@ impl TextureUploadDesc {
         debug_assert!(self.depth > 0);
 
         // Width must be aligned to the row pitch
-        let aligned_width = self
-            .width
-            .next_multiple_of(self.format.buffer_to_texture_copy_row_pitch());
+        let aligned_width = self.aligned_width();
 
         // Use the padded width and bytes-per-texel to calculate the required size
         let texel_count = aligned_width * self.height * self.depth;
         let bytes = self.format.bytes_per_element() * texel_count;
 
         bytes
+    }
+
+    /// Returns the width of the texture expanded to the width required to satisfy row pitch
+    /// requirements needed for uploading the texture with a buffer to texture copy.
+    pub const fn aligned_width(&self) -> u32 {
+        // Width must be aligned to the row pitch
+        let aligned_width = self
+            .width
+            .next_multiple_of(self.format.buffer_to_texture_copy_row_pitch());
+
+        aligned_width
+    }
+
+    pub const fn extent(&self) -> Extent3D {
+        Extent3D::new(self.width, self.height, self.depth)
     }
 }
 
@@ -105,9 +127,8 @@ pub struct TextureUploadSource {
     pub(crate) desc: TextureUploadDesc,
 
     /// An offset in bytes from the start of the buffer for where the upload data starts. This
-    /// is needed as the upload commands use an offset+len and not a raw ptr+len pair. We use [u32]
-    /// because that is the largest width we can send to a platform API for a buffer offset.
-    pub(crate) offset: u32,
+    /// is needed as the upload commands use an offset+len and not a raw ptr+len pair.
+    pub(crate) offset: u64,
 
     /// A pointer to a slice of memory for where the upload data should be written into for the
     /// specific image mip that is being uploaded by this request. This will be appropriately sized
@@ -122,27 +143,34 @@ pub struct TextureUploadSource {
     pub(crate) data: NonNull<[u8]>,
 }
 
+// TODO: Block Formats like DXT/BCn
+
 impl TextureUploadSource {
     /// Constructs a new upload source from the given parameters. Includes some debug validation to
     /// try and detect mistakes.
+    ///
+    /// This function is intended to be used when sub-allocating upload payloads from a larger
+    /// staging buffer allocation.
     ///
     /// # Safety
     ///
     /// There are a bunch of requirements for safely implementing this system.
     ///
-    /// - 'desc' should describe a valid, non-zero sized texture.
     /// - 'data' takes ownership of the underlying buffer
     /// - 'data' must point to memory in the mapped range of 'buffer'
     /// - 'data' must be sized for the texture described by 'desc'
     /// - 'offset' must be aligned to 512 bytes within the buffer
     /// - 'data.len()' combined with 'offset' must not overrun the end of the buffer
+    /// - 'desc.width', 'desc.height', and 'desc.depth' must all be at least 1. No zero-sized
+    ///   textures.
     ///
     /// There are a bunch of debug asserts for these which are only enabled on debug builds, check
-    /// those to see all the requirements.
+    /// those to see all the requirements. Do not violate these requirements as they will not be
+    /// checked in a release build.
     pub(crate) unsafe fn new(
         buffer: AnyArc<dyn IBuffer>,
         desc: TextureUploadDesc,
-        offset: u32,
+        offset: u64,
         data: NonNull<[u8]>,
     ) -> Self {
         #[cfg(debug_assertions)]
@@ -173,13 +201,13 @@ impl TextureUploadSource {
             );
 
             debug_assert!(
-                buffer_desc.size >= offset as u64,
+                buffer_desc.size >= offset,
                 "'offset' ({}) is outside of the buffer (size {})",
                 offset,
                 buffer_desc.size
             );
 
-            let bytes_after_offset = buffer_desc.size - offset as u64;
+            let bytes_after_offset = buffer_desc.size - offset;
             debug_assert!(
                 bytes_after_offset >= required_size as u64,
                 "'buffer' is to small. [{}+{}] overruns the upload buffer (size {})",
@@ -197,6 +225,103 @@ impl TextureUploadSource {
         }
     }
 
+    /// Constructs a new owned [TextureUploadSource] for the given texture upload description.
+    ///
+    /// # Safety
+    ///
+    /// See [TextureUploadSource::new] for more information.
+    ///
+    /// This utility is safer to use than [TextureUploadSource::new] as it guarantees all the buffer
+    /// size and ownership constraints by construction. The only relevant requirements are to
+    /// ensure the [TextureUploadDesc] encodes a valid non-zero-sized texture. That is:
+    ///
+    /// - 'desc.width', 'desc.height', and 'desc.depth' must all be at least 1. No zero-sized
+    ///   textures.
+    pub(crate) unsafe fn new_owned(
+        device: &dyn IDevice,
+        desc: TextureUploadDesc,
+    ) -> Result<Self, BufferCreateError> {
+        let size_requirement = desc.size_requirement();
+        let buffer = device.create_buffer(&BufferDesc {
+            size: size_requirement as u64,
+            cpu_access: CpuAccessMode::Write,
+            usage: ResourceUsageFlags::COPY_SOURCE,
+            name: None,
+        })?;
+
+        let ptr = buffer.map().unwrap();
+
+        let out = Self::new(
+            buffer,
+            desc,
+            0,
+            NonNull::slice_from_raw_parts(ptr, size_requirement as usize),
+        );
+        Ok(out)
+    }
+
+    /// Constructs a [BufferToTextureCopyRegion] that encodes a valid copy command to copy from the
+    /// source buffer into the destination texture at the given mip and array layer.
+    ///
+    /// # Info
+    ///
+    /// We make some assumptions.
+    /// - We only allow uploading color images so the image aspect is always
+    ///   [TextureCopyAspect::Color].
+    /// - We only allow uploading entire mip levels and/or array layers so the origin is always
+    ///   `(0, 0)` and the extent is assumed to cover the entire subresource.
+    pub(crate) const fn get_copy_region(
+        &self,
+        mip_level: u32,
+        array_layer: u32,
+        aspect: TextureCopyAspect,
+    ) -> BufferToTextureCopyRegion {
+        BufferToTextureCopyRegion {
+            src: ImageDataLayout {
+                offset: self.offset as u64,
+                row_pitch: self.desc.aligned_width(),
+                extent: self.desc.extent(),
+            },
+            dst: TextureCopyInfo {
+                mip_level,
+                array_layer,
+                aspect,
+                origin: UOffset3D::new(0, 0, 0),
+                extent: self.desc.extent(),
+            },
+        }
+    }
+
+    /// Gets a slice over the requested row, including padding texels needed for satisfying row
+    /// pitch.
+    ///
+    /// # Safety
+    ///
+    /// The input parameter is not bounds checked and my produce a slice that is out of bounds of
+    /// the upload buffer slice we're sub-slicing from. It is the caller's responsibility to ensure
+    /// that the buffer is valid for the requested row.
+    pub unsafe fn row_ptr(&self, row: u32) -> NonNull<[u8]> {
+        // Calculate the offset to the start of the 'row'th row.
+        let aligned_width = self.desc.aligned_width() as usize;
+        let row_offset = aligned_width * row as usize;
+
+        // We check anyway on debug builds because we can.
+        #[cfg(debug_assertions)]
+        {
+            let row_count = self.desc.height * self.desc.depth;
+            debug_assert!(
+                row < row_count,
+                "Row '{row}' out of bounds of '{row_count}'"
+            );
+        }
+
+        // Make our sub-slice with the new offset+len pair.
+        let base_ptr = self.data.cast::<u8>().as_ptr();
+        let ptr = base_ptr.add(row_offset);
+        let ptr = NonNull::new(ptr as _).unwrap_unchecked();
+        NonNull::slice_from_raw_parts(ptr, aligned_width)
+    }
+
     /// Get the upload block as a raw pointer.
     ///
     /// # Performance Warning
@@ -208,25 +333,6 @@ impl TextureUploadSource {
     /// heavily recommended to only perform sequential writes to these regions.
     pub fn data_ptr(&self) -> NonNull<[u8]> {
         self.data
-    }
-
-    /// Get the upload block as a slice.
-    ///
-    /// # Performance Warning
-    ///
-    /// The upload memory may be write-combined or uncached memory as it will be mapped for upload
-    /// to the GPU. Reads should be treated as *very* expensive for these mapped regions.
-    ///
-    /// It is recommended to use write-only accessors to prevent accidental reads. It is also
-    /// heavily recommended to only perform sequential writes to these regions.
-    ///
-    /// This is provided as it is technically valid usage but should be handled with care. Avoid
-    /// reading from this slice (which is the only thing you can do with a shared slice, so just
-    /// don't use this API).
-    pub fn data_ref(&self) -> &[u8] {
-        // Safety: It is guaranteed by the implementation that this should be uniquely owned by the
-        //         request and valid for access as long as the upload request object is available.
-        unsafe { self.data.as_ref() }
     }
 
     /// Get the upload block as a slice.
