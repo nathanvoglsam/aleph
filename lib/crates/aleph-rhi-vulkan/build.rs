955090e8fb20e560d8a1a6f0ba1de59cf262c62e
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2024-06-14 20:10:41 +1000

    [aleph-rhi-vulkan] Linking against iOS vulkan loader

---
+++ lib/crates/aleph-rhi-vulkan/build.rs
@@ -0,0 +1,11 @@
+extern crate aleph_target_build as target;
+
+pub fn main() {
+    let target_platform = target::build::target_platform();
+    if target_platform.is_ios() {
+        println!(
+            "cargo:rustc-link-search=framework={}",
+            "/Users/nvoglsam/VulkanSDK/1.3.283.0/iOS/lib"
+        );
+    }
+}
