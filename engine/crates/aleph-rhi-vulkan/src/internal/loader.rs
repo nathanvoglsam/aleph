cd6fa90812a5f17d34edf9ca8773342ba588f30a
Nathan Voglsam <nathan.p.voglsam@gmail.com>
2024-10-21 14:41:56 +1100

    [aleph-rhi-vulkan] Improve our Vulkan loader search code to allow searching under multiple names
    MacOS in particular as issues finding 'libvulkan.dylib' when running in debuggers and an absolute path is needed instead.

--- engine/crates/aleph-rhi-vulkan/src/internal/loader.rs
+++ engine/crates/aleph-rhi-vulkan/src/internal/loader.rs
@@ -27,23 +27,28 @@
 // SOFTWARE.
 //
 
-pub const fn platform_library_name() -> &'static str {
-    #[cfg(windows)]
-    const LIB_PATH: &str = "vulkan-1.dll";
-
-    #[cfg(all(
-        unix,
-        not(any(target_os = "macos", target_os = "ios", target_os = "android"))
-    ))]
-    const LIB_PATH: &str = "libvulkan.so.1";
-
-    #[cfg(target_os = "android")]
-    const LIB_PATH: &str = "libvulkan.so";
-
-    #[cfg(any(target_os = "macos", target_os = "ios"))]
-    const LIB_PATH: &str = "libvulkan.dylib";
-
-    LIB_PATH
+pub const fn platform_library_search_stack() -> &'static [&'static str] {
+    if cfg!(windows) {
+        // Windows
+        &["vulkan-1.dll"]
+    } else if cfg!(target_os = "macos") {
+        // MacOS via MoltenVK, has two paths to search incase 'libvulkan.dylib' isn't in the rpath.
+        // This is a workaround as the game can't find 'libvulkan.dylib' when running in some dev
+        // tools (debuggers) otherwise. We use the absolute path if the first fails.
+        &["libvulkan.dylib", "/usr/local/lib/libvulkan.dylib"]
+    } else if cfg!(target_os = "ios") {
+        // IOS doesn't fall victim to the same flaw because we're in an app bundle, and there's no
+        // global Vulkan loader to find anyway.
+        &["libvulkan.dylib"]
+    } else if cfg!(target_os = "android") {
+        // Android matches cfg!(unix) but has a different name than linux typically uses
+        &["libvulkan.so"]
+    } else if cfg!(unix) {
+        // This covers linux and friends with a spread of possible loader library names
+        &["libvulkan.so.1", "libvulkan.so"]
+    } else {
+        panic!("Unsupported platform!");
+    }
 }
 
 #[cfg(not(target_os = "ios"))]
@@ -67,7 +72,13 @@ pub unsafe fn load() -> Option<(LibraryType, ash::Entry)> {
         Some((lib, ash::Entry::from_static_fn(static_fn)))
     }
 
-    load_from(platform_library_name())
+    for lib in platform_library_search_stack() {
+        let result = load_from(lib);
+        if result.is_some() {
+            return result;
+        }
+    }
+    None
 }
 
 #[cfg(target_os = "ios")]
